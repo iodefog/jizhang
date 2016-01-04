fecb6328e089159d183f52d1a5621d70c0439498
赵天立 <125037896@qq.com>
2016-01-04 09:33:24 +0800

    资金页面

---
+++ SuiShouJi/Module/Financing/FinancingHome/Model/SSJFinancingHomeitem.h
@@ -0,0 +1,30 @@
+//
+//  SSJFinancingHomeitem.h
+//  SuiShouJi
+//
+//  Created by 赵天立 on 16/1/3.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSJFinancingHomeitem : NSObject
+
+//账户名称
+@property (nonatomic,strong) NSString *fundingName;
+
+//账户颜色
+@property (nonatomic,strong) NSString *fundingColor;
+
+//账户ID
+@property (nonatomic,strong) NSString *fundingID;
+
+//账户图标
+@property (nonatomic,strong) NSString *fundingIcon;
+
+//账户父类
+@property (nonatomic,strong) NSString *fundingParent;
+
+//账户余额
+@property (nonatomic) double fundingAmount;
+@end
--- SuiShouJi/Module/Financing/FinancingHome/ViewController/SSJFinancingHomeViewController.h
+++ SuiShouJi/Module/Financing/FinancingHome/ViewController/SSJFinancingHomeViewController.h
@@ -8,6 +8,6 @@
 
 #import "SSJBaseViewController.h"
 
-@interface SSJFinancingHomeViewController : SSJBaseViewController
+@interface SSJFinancingHomeViewController : SSJBaseViewController<UICollectionViewDataSource,UICollectionViewDelegate>
 
 @end
