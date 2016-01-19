d84b5b817089a4c57307796caf0d826d0d7ae78c
赵天立 <125037896@qq.com>
2016-01-19 19:56:07 +0800

    bug fixed

---
+++ SuiShouJi/Module/BookKeeping/Calerdar/ViewController/SSJCalenderDetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  SSJCalenderDetailViewController.h
+//  SuiShouJi
+//
+//  Created by 赵天立 on 16/1/19.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJNewBaseTableViewController.h"
+#import "SSJBillingChargeCellItem.h"
+
+@interface SSJCalenderDetailViewController :SSJNewBaseTableViewController
+@property (nonatomic,strong) SSJBillingChargeCellItem *item;
+@end
