e1871cc1380d0a8594f5f3ba8b3a2a4a769c390c
old lang <langxintong@9188.com>
2016-04-01 13:41:01 +0800

    数据库升级组建更新

---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion2.m
@@ -0,0 +1,40 @@
+//
+//  SSJDatabaseVersion2.m
+//  SuiShouJi
+//
+//  Created by old lang on 16/4/1.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJDatabaseVersion2.h"
+#import <FMDB/FMDB.h>
+
+@implementation SSJDatabaseVersion2
+
++ (NSError *)startUpgradeInDatabase:(FMDatabase *)db {
+    NSError *error = nil;
+    error = [self upgradeUserTableWithDatabase:db];
+    error = [self createUserTreeWithDatabase:db];
+    return error;
+}
+
+//  更新用户表
++ (NSError *)upgradeUserTableWithDatabase:(FMDatabase *)db {
+    NSError *error = nil;
+    if (![db columnExists:@"usersignaguture" inTableWithName:@"bk_user"]) {
+        if (![db executeUpdate:@"alter table bk_user add usersignaguture text"]) {
+            error = [db lastError];
+        }
+    }
+    
+    return error;
+}
+
++ (NSError *)createUserTreeWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"create table if not exists bk_user_tree (cuserid text not null, isignin integer not null, isignindate text not null, hasshaked integer default 0)"]) {
+        return [db lastError];
+    }
+    return nil;
+}
+
+@end
