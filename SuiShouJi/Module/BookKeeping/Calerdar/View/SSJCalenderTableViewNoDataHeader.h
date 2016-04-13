0cbae987bae928ca423a2eb9608fd5e56dc0723d
ricky <125037896@qq.com>
2016-04-13 15:35:01 +0800

    细节

--- SuiShouJi/Module/BookKeeping/Calerdar/View/SSJCalenderTableViewNoDataHeader.h
+++ SuiShouJi/Module/BookKeeping/Calerdar/View/SSJCalenderTableViewNoDataHeader.h
@@ -11,8 +11,10 @@
 @interface SSJCalenderTableViewNoDataHeader : UIView
 
 + (id)CalenderTableViewNoDataHeader;
+
 //点击记录按钮的回调
 typedef void (^RecordMakingButtonBlock)();
 
 @property (nonatomic, copy) RecordMakingButtonBlock RecordMakingButtonBlock;
+
 @end
