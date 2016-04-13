8a77d3e45c71ad256f4622b2c40e7979e01d6aff
old lang <langxintong@9188.com>
2016-04-13 18:45:52 +0800

    no message

--- SuiShouJi/Components/Animation/UIView+SSJViewAnimatioin.m
+++ SuiShouJi/Components/Animation/UIView+SSJViewAnimatioin.m
@@ -14,7 +14,7 @@ static const void *kSSJViewAnimatioinShowedKey = &kSSJViewAnimatioinShowedKey;
 
 @implementation UIView (SSJViewAnimatioin)
 
-- (UIView *)backgroundView {
+- (UIView *)ssj_backgroundView {
     UIView *background = objc_getAssociatedObject(self, kSSJViewAnimatioinBackgroundViewKey);
     if (!background) {
         background = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -24,30 +24,30 @@ static const void *kSSJViewAnimatioinShowedKey = &kSSJViewAnimatioinShowedKey;
     return background;
 }
 
-- (BOOL)showed {
+- (BOOL)ssj_showed {
     return [objc_getAssociatedObject(self, kSSJViewAnimatioinShowedKey) boolValue];
 }
 
-- (void)setShowed:(BOOL)showed {
+- (void)ssj_setShowed:(BOOL)showed {
     objc_setAssociatedObject(self, kSSJViewAnimatioinShowedKey, @(showed), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
-- (void)popupInView:(UIView *)view completion:(void (^)())completion {
+- (void)ssj_popupInView:(UIView *)view completion:(void (^)())completion {
     if (self == view) {
         return;
     }
-    if (self.showed) {
+    if (self.ssj_showed) {
         return;
     }
     
     if (self.superview != view) {
-        [view addSubview:self.backgroundView];
+        [view addSubview:self.ssj_backgroundView];
         [view addSubview:self];
     }
     
     self.center = CGPointMake(view.width * 0.5, view.height * 0.5);
     self.transform = CGAffineTransformMakeScale(0, 0);
-    self.backgroundView.alpha = 0;
+    self.ssj_backgroundView.alpha = 0;
     
     [UIView animateKeyframesWithDuration:0.36 delay:0 options:UIViewKeyframeAnimationOptionCalculationModeLinear animations:^{
         [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.25 animations:^{
@@ -62,16 +62,12 @@ static const void *kSSJViewAnimatioinShowedKey = &kSSJViewAnimatioinShowedKey;
         [UIView addKeyframeWithRelativeStartTime:0.75 relativeDuration:0.25 animations:^{
             self.transform = CGAffineTransformMakeScale(1, 1);
         }];
-        self.backgroundView.alpha = 0.3;
-    } completion:NULL];
-}
-
-- (void)popup {
-    
-}
-
-- (void)showBackground {
-    
+        self.ssj_backgroundView.alpha = 0.3;
+    } completion:^(BOOL finished) {
+        if (completion) {
+            completion();
+        }
+    }];
 }
 
 - (void)dismiss:(void (^)())completion {
