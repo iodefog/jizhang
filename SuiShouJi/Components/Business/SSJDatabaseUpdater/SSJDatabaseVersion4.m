7ccb885a7b6a6666c8e03906e393ecadf0403779
old lang <langxintong@9188.com>
2016-05-30 15:29:21 +0800

    数据库升级组件

--- SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion4.m
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion4.m
@@ -16,14 +16,62 @@
     if (error) {
         return error;
     }
+    
+    error = [self updateUserChargeTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
+    error = [self updateUserBudgetTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
+    error = [self updateDailySumChargeTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
+    error = [self updateChargePeriodConfigTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
     return nil;
 }
 
 + (NSError *)createBooksTypeTableWithDatabase:(FMDatabase *)db {
-    if (![db executeUpdate:@"create table if not exists bk_books_type (cbooksid text not null, cbooksname text not null, cbookscolor text, cwritedate text, operatortype integer, iversion integer, cuserid text, primary key(cbooksid))"]) {
+    if (![db executeUpdate:@"create table if not exists bk_books_type (cbooksid text not null, cbooksname text not null, cbookscolor text, cwritedate text, operatortype integer, iversion integer, cuserid text, primary key(cbooksid, cuserid))"]) {
         return [db lastError];
     }
-    
+    return nil;
+}
+
++ (NSError *)updateUserChargeTableWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"alter table bk_user_charge add cbooksid text default '0'"]) {
+        return [db lastError];
+    }
+    return nil;
+}
+
++ (NSError *)updateUserBudgetTableWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"alter table bk_user_budget add cbooksid text default '0'"]) {
+        return [db lastError];
+    }
+    return nil;
+}
+
++ (NSError *)updateDailySumChargeTableWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"alter table bk_dailysum_charge add cbooksid text default '0'"]) {
+        return [db lastError];
+    }
+    return nil;
+}
+
++ (NSError *)updateChargePeriodConfigTableWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"alter table bk_charge_period_config add cbooksid text default '0'"]) {
+        return [db lastError];
+    }
     return nil;
 }
 
