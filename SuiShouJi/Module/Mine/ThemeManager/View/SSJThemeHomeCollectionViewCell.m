0dcebdfb08aff8efac68cb642acb6f2040f50356
ricky <125037896@qq.com>
2016-07-04 17:58:50 +0800

    bug

--- SuiShouJi/Module/Mine/ThemeManager/View/SSJThemeHomeCollectionViewCell.m
+++ SuiShouJi/Module/Mine/ThemeManager/View/SSJThemeHomeCollectionViewCell.m
@@ -125,7 +125,7 @@
 
 -(void)statusButtonClicked:(id)sender{
 //    __weak typeof(self) weakSelf = self;
-//    if([((UIButton *)sender).titleLabel.text isEqualToString:@"下载"]) {
+    if([((UIButton *)sender).titleLabel.text isEqualToString:@"下载"]) {
         [((UIButton *)sender) setTitle:@"" forState:UIControlStateNormal];
         [[SSJThemeDownLoaderManger sharedInstance] downloadThemeWithID:self.item.themeId url:self.item.downLoadUrl success:^{
             
@@ -134,7 +134,7 @@
         }];
         self.themeStatusButton.downloadMaskView.hidden = NO;
         [[SSJThemeDownLoaderManger sharedInstance] addProgressHandler:_downloadHandler forID:self.item.themeId];
-//    }
+    }
 }
 
 -(void)setItem:(SSJThemeItem *)item{
