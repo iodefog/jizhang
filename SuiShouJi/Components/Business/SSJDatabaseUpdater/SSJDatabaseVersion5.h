6299036cb337fb6056e81cf9b2e798c8e6fda578
old lang <langxintong@9188.com>
2016-07-11 10:48:00 +0800

    no message

---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion5.h
@@ -0,0 +1,14 @@
+//
+//  SSJDatabaseVersion5.h
+//  SuiShouJi
+//
+//  Created by old lang on 16/7/11.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SSJDatabaseVersionProtocol.h"
+
+@interface SSJDatabaseVersion5 : NSObject <SSJDatabaseVersionProtocol>
+
+@end
---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion5.m
@@ -0,0 +1,30 @@
+//
+//  SSJDatabaseVersion5.m
+//  SuiShouJi
+//
+//  Created by old lang on 16/7/11.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJDatabaseVersion5.h"
+#import <FMDB/FMDB.h>
+
+@implementation SSJDatabaseVersion5
+
++ (NSError *)startUpgradeInDatabase:(FMDatabase *)db {
+    NSError *error = [self updateUserTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
+    return nil;
+}
+
++ (NSError *)updateUserTableWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"alter table bk_user add logintype integer"]) {
+        return [db lastError];
+    }
+    return nil;
+}
+
+@end
