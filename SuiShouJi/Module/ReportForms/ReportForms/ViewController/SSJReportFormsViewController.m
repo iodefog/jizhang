fe3ee7d6cf501e47f1c58648eee4d0296d0baffd
old lang <langxintong@9188.com>
2016-07-12 18:25:32 +0800

    no message

--- SuiShouJi/Module/ReportForms/ReportForms/ViewController/SSJReportFormsViewController.m
+++ SuiShouJi/Module/ReportForms/ReportForms/ViewController/SSJReportFormsViewController.m
@@ -131,6 +131,7 @@ static NSString *const kSegmentTitleSurplus = @"结余";
     
     _tableView.separatorColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha];
     
+    _chartView.contentView.backgroundColor = [UIColor ssj_colorWithHex:@"#FFFFFF" alpha:SSJ_CURRENT_THEME.backgroundAlpha];
     [_chartView ssj_setBorderColor:[UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha]];
     
     [_surplusView updateThemeColor];
