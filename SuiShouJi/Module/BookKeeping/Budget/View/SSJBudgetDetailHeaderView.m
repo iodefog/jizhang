67fd8db981c0a47d1ce08f34353003734bcf1474
old lang <langxintong@9188.com>
2016-08-01 18:30:35 +0800

    预算详情样式调整

--- SuiShouJi/Module/BookKeeping/Budget/View/SSJBudgetDetailHeaderView.m
+++ SuiShouJi/Module/BookKeeping/Budget/View/SSJBudgetDetailHeaderView.m
@@ -10,6 +10,10 @@
 #import "SSJBudgetWaveWaterView.h"
 #import "SSJBudgetModel.h"
 
+static const CGFloat kTopViewHeight = 110;
+
+static const CGFloat kBottomViewHeight = 265;
+
 @interface SSJBudgetDetailHeaderView ()
 
 //  包涵本周期预算金额、据结算日天数的视图
@@ -29,6 +33,8 @@
 
 //  顶部的据结算日天数
 @property (nonatomic, strong) UILabel *intervalLab;
+
+@property (nonatomic, strong) CAShapeLayer *dashLine;
 
 //  波浪比例视图
 @property (nonatomic, strong) SSJBudgetWaveWaterView *waveView;
@@ -64,7 +70,7 @@
 
 - (CGSize)sizeThatFits:(CGSize)size {
     CGFloat width = CGRectGetWidth([UIScreen mainScreen].bounds);
-    CGFloat height = self.isHistory ? 152 : 295;
+    CGFloat height = self.isHistory ? kBottomViewHeight : kTopViewHeight + kBottomViewHeight;
     return CGSizeMake(width, height);
 }
 
@@ -75,7 +81,7 @@
     self.waveView.centerX = self.width * 0.5;
     
     if (self.isHistory) {
-        self.bottomView.frame = CGRectMake(0, 0, self.width, 200);
+        self.bottomView.frame = CGRectMake(0, 0, self.width, kBottomViewHeight);
         
         self.payMoneyLab.width = MIN(self.payMoneyLab.width, (self.width - 80) * 0.5);
         self.estimateMoneyLab.width = MIN(self.estimateMoneyLab.width, (self.width - 80) * 0.5);
@@ -83,8 +89,8 @@
         self.payMoneyLab.left = 30;
         self.estimateMoneyLab.right = self.width - 30;
     } else {
-        self.topView.frame = CGRectMake(0, 0, self.width, 95);
-        self.bottomView.frame = CGRectMake(0, self.topView.bottom, self.width, 200);
+        self.topView.frame = CGRectMake(0, 0, self.width, kTopViewHeight);
+        self.bottomView.frame = CGRectMake(0, self.topView.bottom, self.width, kBottomViewHeight);
         
         CGFloat top1 = (self.topView.height - self.budgetMoneyTitleLab.height - self.budgetMoneyLab.height - gap) * 0.5;
         
@@ -102,6 +108,9 @@
         self.payMoneyLab.top = self.waveView.bottom + 15;
         self.bottomLab.top = self.payMoneyLab.bottom + 13;
         self.payMoneyLab.centerX = self.bottomLab.centerX = self.width * 0.5;
+        
+        self.dashLine.left = self.topView.width * 0.5;
+        self.dashLine.top = 30;
     }
 }
 
@@ -144,6 +153,7 @@
     
     self.waveView.percent = (model.payMoney / model.budgetMoney);
     self.waveView.money = model.budgetMoney - model.payMoney;
+    self.topView.backgroundColor = self.waveView.percent <= 1 ? [UIColor ssj_colorWithHex:@"0fceb6"] : [UIColor ssj_colorWithHex:@"ff654c"];
     
     self.payMoneyLab.text = [NSString stringWithFormat:@"已花：%.2f", model.payMoney];
     [self.payMoneyLab sizeToFit];
@@ -155,13 +165,13 @@
     if (balance >= 0) {
         NSString *money = [NSString stringWithFormat:@"%.2f", balance / interval];
         NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"距结算日前，您每天还可花%@元哦", money]];
-        [text setAttributes:@{NSForegroundColorAttributeName:[UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor]} range:NSMakeRange(12, money.length)];
+        [text setAttributes:@{NSForegroundColorAttributeName:[UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor]} range:NSMakeRange(12, money.length)];
         self.bottomLab.attributedText = text;
         [self.bottomLab sizeToFit];
     } else {
         NSString *money = [NSString stringWithFormat:@"%.2f", ABS(balance)];
         NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"亲爱的小主，您目前已超支%@元喽", money]];
-        [text setAttributes:@{NSForegroundColorAttributeName:[UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor]} range:NSMakeRange(12, money.length)];
+        [text setAttributes:@{NSForegroundColorAttributeName:[UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor]} range:NSMakeRange(12, money.length)];
         self.bottomLab.attributedText = text;
         [self.bottomLab sizeToFit];
     }
@@ -170,11 +180,11 @@
 - (UIView *)topView {
     if (!_topView) {
         _topView = [[UIView alloc] init];
-        _topView.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor];
         [_topView addSubview:self.budgetMoneyTitleLab];
         [_topView addSubview:self.budgetMoneyLab];
         [_topView addSubview:self.intervalTitleLab];
         [_topView addSubview:self.intervalLab];
+        [_topView.layer addSublayer:self.dashLine];
     }
     return _topView;
 }
@@ -194,9 +204,9 @@
 - (UILabel *)budgetMoneyTitleLab {
     if (!_budgetMoneyTitleLab) {
         _budgetMoneyTitleLab = [[UILabel alloc] init];
-        _budgetMoneyTitleLab.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor];
+        _budgetMoneyTitleLab.backgroundColor = [UIColor clearColor];
         _budgetMoneyTitleLab.textColor = [UIColor whiteColor];
-        _budgetMoneyTitleLab.font = [UIFont systemFontOfSize:12];
+        _budgetMoneyTitleLab.font = [UIFont systemFontOfSize:13];
     }
     return _budgetMoneyTitleLab;
 }
@@ -204,9 +214,10 @@
 - (UILabel *)budgetMoneyLab {
     if (!_budgetMoneyLab) {
         _budgetMoneyLab = [[UILabel alloc] init];
-        _budgetMoneyLab.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor];
+        _budgetMoneyLab.backgroundColor = [UIColor clearColor];
         _budgetMoneyLab.textColor = [UIColor whiteColor];
-        _budgetMoneyLab.font = [UIFont systemFontOfSize:18];
+        _budgetMoneyLab.font = [UIFont systemFontOfSize:27];
+        _budgetMoneyLab.adjustsFontSizeToFitWidth = YES;
     }
     return _budgetMoneyLab;
 }
@@ -214,9 +225,9 @@
 - (UILabel *)intervalTitleLab {
     if (!_intervalTitleLab) {
         _intervalTitleLab = [[UILabel alloc] init];
-        _intervalTitleLab.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor];
+        _intervalTitleLab.backgroundColor = [UIColor clearColor];
         _intervalTitleLab.textColor = [UIColor whiteColor];
-        _intervalTitleLab.font = [UIFont systemFontOfSize:12];
+        _intervalTitleLab.font = [UIFont systemFontOfSize:13];
         _intervalTitleLab.text = @"距结算日";
         [_intervalTitleLab sizeToFit];
     }
@@ -226,25 +237,41 @@
 - (UILabel *)intervalLab {
     if (!_intervalLab) {
         _intervalLab = [[UILabel alloc] init];
-        _intervalLab.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.buttonColor];
+        _intervalLab.backgroundColor = [UIColor clearColor];
         _intervalLab.textColor = [UIColor whiteColor];
-        _intervalLab.font = [UIFont systemFontOfSize:18];
+        _intervalLab.font = [UIFont systemFontOfSize:27];
     }
     return _intervalLab;
 }
 
+- (CAShapeLayer *)dashLine {
+    if (!_dashLine) {
+        UIBezierPath *path = [UIBezierPath bezierPath];
+        [path moveToPoint:CGPointZero];
+        [path addLineToPoint:CGPointMake(0, 52)];
+        
+        _dashLine = [CAShapeLayer layer];
+        _dashLine.lineDashPattern = @[@4, @4];
+        _dashLine.lineWidth = 1 / [UIScreen mainScreen].scale;
+        _dashLine.fillColor = [UIColor whiteColor].CGColor;
+        _dashLine.strokeColor = [UIColor whiteColor].CGColor;
+        _dashLine.path = path.CGPath;
+    }
+    return _dashLine;
+}
+
 - (SSJBudgetWaveWaterView *)waveView {
     if (!_waveView) {
-        _waveView = [[SSJBudgetWaveWaterView alloc] initWithRadius:90];
-        _waveView.waveAmplitude = 6;
-        _waveView.waveSpeed = 4;
+        _waveView = [[SSJBudgetWaveWaterView alloc] initWithRadius:160];
+        _waveView.waveAmplitude = 12;
+        _waveView.waveSpeed = 5;
         _waveView.waveCycle = 1;
-        _waveView.waveGrowth = 2;
-        _waveView.waveOffset = 40;
-        _waveView.fullWaveAmplitude = 5;
-        _waveView.fullWaveSpeed = 3;
+        _waveView.waveGrowth = 3;
+        _waveView.waveOffset = 60;
+        _waveView.fullWaveAmplitude = 8;
+        _waveView.fullWaveSpeed = 4;
         _waveView.fullWaveCycle = 4;
-        _waveView.outerBorderWidth = 5;
+        _waveView.outerBorderWidth = 8;
         _waveView.showText = YES;
     }
     return _waveView;
@@ -255,7 +282,8 @@
         _payMoneyLab = [[UILabel alloc] init];
         _payMoneyLab.backgroundColor = [UIColor clearColor];
         _payMoneyLab.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor];
-        _payMoneyLab.font = [UIFont systemFontOfSize:14];
+        _payMoneyLab.font = [UIFont systemFontOfSize:20];
+        _payMoneyLab.adjustsFontSizeToFitWidth = YES;
     }
     return _payMoneyLab;
 }
@@ -264,8 +292,10 @@
     if (!_estimateMoneyLab) {
         _estimateMoneyLab = [[UILabel alloc] init];
         _estimateMoneyLab.backgroundColor = [UIColor clearColor];
+        _estimateMoneyLab.textAlignment = NSTextAlignmentRight;
         _estimateMoneyLab.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor];
-        _estimateMoneyLab.font = [UIFont systemFontOfSize:14];
+        _estimateMoneyLab.font = [UIFont systemFontOfSize:20];
+        _estimateMoneyLab.adjustsFontSizeToFitWidth = YES;
     }
     return _estimateMoneyLab;
 }
@@ -275,7 +305,7 @@
         _bottomLab = [[UILabel alloc] init];
         _bottomLab.backgroundColor = [UIColor clearColor];
         _bottomLab.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.secondaryColor];
-        _bottomLab.font = [UIFont systemFontOfSize:12];
+        _bottomLab.font = [UIFont systemFontOfSize:13];
     }
     return _bottomLab;
 }
