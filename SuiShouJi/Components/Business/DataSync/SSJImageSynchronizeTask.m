f149e98175076507444ce595144436b764bf0574
old lang <langxintong@9188.com>
2016-08-09 17:10:26 +0800

    no message

--- SuiShouJi/Components/Business/DataSync/SSJImageSynchronizeTask.m
+++ SuiShouJi/Components/Business/DataSync/SSJImageSynchronizeTask.m
@@ -28,8 +28,26 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
     
     [[SSJDatabaseQueue sharedInstance] inDatabase:^(FMDatabase *db) {
         
-        //  查询当前用户没有同步的图片
+        //  查询当前用户的流水表中没有同步的图片
         FMResultSet *resultSet = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_user_charge as b where a.rid = b.ichargeid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
+        if (!resultSet) {
+            if (failure) {
+                failure([db lastError]);
+            }
+            return;
+        }
+        
+        while ([resultSet next]) {
+            NSString *imageName = [resultSet stringForColumn:@"cimgname"];
+            NSString *syncType = [resultSet stringForColumn:@"isynctype"];
+            if (imageName.length) {
+                [imageInfoArr addObject:@{kImageNameKey:imageName,
+                                          kSyncTypeKey:syncType}];
+            }
+        }
+        
+        //  查询当前用户的顶起记账中没有同步的图片
+        resultSet = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_charge_operiod_config as b where a.rid = b.iconfigid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
         if (!resultSet) {
             if (failure) {
                 failure([db lastError]);
---
+++ SuiShouJi/Module/Mine/MagicExport/View/SSJMagicExportSuccessAlertView.m
@@ -0,0 +1,21 @@
+//
+//  SSJMagicExportSuccessAlertView.m
+//  SuiShouJi
+//
+//  Created by old lang on 16/8/9.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJMagicExportSuccessAlertView.h"
+
+@implementation SSJMagicExportSuccessAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
