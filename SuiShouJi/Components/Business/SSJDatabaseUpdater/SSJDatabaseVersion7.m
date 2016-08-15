d067627e0b0f456fa88ad129c70573630d03b1e7
old lang <langxintong@9188.com>
2016-08-15 17:38:29 +0800

    修复数据格式化后同步失败

---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion7.m
@@ -0,0 +1,30 @@
+//
+//  SSJDatabaseVersion7.m
+//  SuiShouJi
+//
+//  Created by old lang on 16/8/15.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJDatabaseVersion7.h"
+
+@implementation SSJDatabaseVersion7
+
++ (NSError *)startUpgradeInDatabase:(FMDatabase *)db {
+    NSError *error = [self updateUserTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
+    return nil;
+}
+
++ (NSError *)updateUserTableWithDatabase:(FMDatabase *)db {
+    if (![db executeUpdate:@"alter table bk_user add lastselectfundid text"]) {
+        return [db lastError];
+    }
+    
+    return nil;
+}
+
+@end
