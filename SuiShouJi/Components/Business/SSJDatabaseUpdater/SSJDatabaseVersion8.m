95f8692dac2de7ffe697879ee30a3309930264fe
old lang <langxintong@9188.com>
2016-08-16 16:36:30 +0800

    no message

---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion8.m
@@ -0,0 +1,13 @@
+//
+//  SSJDatabaseVersion8.m
+//  SuiShouJi
+//
+//  Created by old lang on 16/8/16.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJDatabaseVersion8.h"
+
+@implementation SSJDatabaseVersion8
+
+@end
