6775ffe333af2e91ce6ad725f84ce78f8745341f
old lang <langxintong@9188.com>
2016-08-17 17:28:11 +0800

    借贷存储、查询逻辑

--- SuiShouJi/Module/Financing/Loan/Model/SSJLoanModel.h
+++ SuiShouJi/Module/Financing/Loan/Model/SSJLoanModel.h
@@ -8,6 +8,60 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, SSJLoanType) {
+    SSJLoanTypeLend,    // 借出
+    SSJLoanTypeBorrow   // 借入
+};
+
 @interface SSJLoanModel : NSObject
+
+@property (nonatomic, copy) NSString *ID;
+
+// 用户ID
+@property (nonatomic, copy) NSString *userID;
+
+// 借款人／欠款人
+@property (nonatomic, copy) NSString *lender;
+
+// 借入／借出金额
+@property (nonatomic, copy) NSString *jMoney;
+
+// 借入／借出所属账户
+@property (nonatomic, copy) NSString *fundID;
+
+// 借入／借出目标账户
+@property (nonatomic, copy) NSString *targetFundID;
+
+// 借入／借出日期
+@property (nonatomic, copy) NSString *borrowDate;
+
+// 期限日期
+@property (nonatomic, copy) NSString *repaymentDate;
+
+// 利率
+@property (nonatomic, copy) NSString *rate;
+
+// 备注
+@property (nonatomic, copy) NSString *memo;
+
+// 提醒ID
+@property (nonatomic, copy) NSString *remindID;
+
+// 是否计息
+@property (nonatomic) BOOL interest;
+
+// 是否已结清
+@property (nonatomic) BOOL closeOut;
+
+// 0:借出 1:借入
+@property (nonatomic) SSJLoanType type;
+
+@property (nonatomic) int operatorType;
+
+@property (nonatomic) long long version;
+
+@property (nonatomic, copy) NSString *writeDate;
+
++ (NSDictionary *)propertyMapping;
 
 @end
--- SuiShouJi/Module/Financing/Loan/Model/SSJLoanModel.m
+++ SuiShouJi/Module/Financing/Loan/Model/SSJLoanModel.m
@@ -10,4 +10,44 @@
 
 @implementation SSJLoanModel
 
++ (NSDictionary *)propertyMapping {
+    return @{@"ID":@"loanid",
+             @"userID":@"cuserid",
+             @"lender":@"lender",
+             @"jMoney":@"jmoney",
+             @"fundID":@"cthefundid",
+             @"targetFundID":@"ctargetfundid",
+             @"borrowDate":@"cborrowdate",
+             @"repaymentDate":@"crepaymentdate",
+             @"rate":@"rate",
+             @"memo":@"memo",
+             @"remindID":@"cremindid",
+             @"interest":@"interest",
+             @"closeOut":@"iend",
+             @"type":@"itype",
+             @"operatorType":@"operatortype",
+             @"version":@"iversion",
+             @"writeDate":@"cwriteDate"};
+}
+
+- (NSString *)debugDescription {
+    return [NSString stringWithFormat:@"%@", @{@"ID":(_ID ?: @""),
+                                               @"userID":(_userID ?: @""),
+                                               @"lender":(_lender ?: @""),
+                                               @"jMoney":(_jMoney ?: @""),
+                                               @"fundID":(_fundID ?: @""),
+                                               @"targetFundID":(_targetFundID ?: @""),
+                                               @"borrowDate":(_borrowDate ?: @""),
+                                               @"repaymentDate":(_repaymentDate ?: @""),
+                                               @"rate":(_rate ?: @""),
+                                               @"memo":(_memo ?: @""),
+                                               @"remindID":(_remindID ?: @""),
+                                               @"interest":@(_interest),
+                                               @"closeOut":@(_closeOut),
+                                               @"type":@(_type),
+                                               @"operatorType":@(_operatorType),
+                                               @"version":@(_version),
+                                               @"writeDate":(_writeDate ?: @"")}];
+}
+
 @end
