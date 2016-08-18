7c7483aa52f5b3e36091d813ae55483972363ac7
old lang <langxintong@9188.com>
2016-08-18 16:05:39 +0800

    no message

--- SuiShouJi/Components/Business/DataSync/SyncTable/SSJLoanSyncTable.m
+++ SuiShouJi/Components/Business/DataSync/SyncTable/SSJLoanSyncTable.m
@@ -10,4 +10,20 @@
 
 @implementation SSJLoanSyncTable
 
++ (NSString *)tableName {
+    return @"bk_loan";
+}
+
++ (NSArray *)columns {
+    return @[@"loanid", @"cuserid", @"lender", @"jmoney", @"cthefundid", @"ctargetfundid", @"cthecharge", @"ctargetcharge", @"cborrowdate", @"crepaymentdate", @"cenddate", @"rate", @"memo", @"cremindid", @"interest", @"iend", @"itype", @"operatorType", @"iversion", @"cwritedate"];
+}
+
++ (NSArray *)primaryKeys {
+    return @[@"loanid"];
+}
+
++ (BOOL)shouldMergeRecord:(NSDictionary *)record forUserId:(NSString *)userId inDatabase:(FMDatabase *)db error:(NSError **)error {
+    return [db boolForQuery:@"select count(*) from BK_FUND_INFO where CFUNDID = ?", record[@"cparent"]];
+}
+
 @end
