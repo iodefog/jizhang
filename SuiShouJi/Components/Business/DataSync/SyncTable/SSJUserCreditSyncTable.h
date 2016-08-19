93555ad61fe57593547a60397ae3647975db33c7
old lang <langxintong@9188.com>
2016-08-19 14:55:21 +0800

    提醒表、借贷表、信用卡同步

---
+++ SuiShouJi/Components/Business/DataSync/SyncTable/SSJUserCreditSyncTable.h
@@ -0,0 +1,13 @@
+//
+//  SSJUserCreditSyncTable.h
+//  SuiShouJi
+//
+//  Created by old lang on 16/8/19.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJBaseSyncTable.h"
+
+@interface SSJUserCreditSyncTable : SSJBaseSyncTable
+
+@end
---
+++ SuiShouJi/Components/Business/DataSync/SyncTable/SSJUserCreditSyncTable.m
@@ -0,0 +1,32 @@
+//
+//  SSJUserCreditSyncTable.m
+//  SuiShouJi
+//
+//  Created by old lang on 16/8/19.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import "SSJUserCreditSyncTable.h"
+
+@implementation SSJUserCreditSyncTable
+
++ (NSString *)tableName {
+    return @"bk_user_credit";
+}
+
++ (NSArray *)columns {
+    return @[@"cfundid",
+             @"iquota",
+             @"cbilldate",
+             @"crepaymentdate",
+             @"cremindid",
+             @"cwritedate",
+             @"iversion",
+             @"operatortype"];
+}
+
++ (NSArray *)primaryKeys {
+    return @[@"cfundid"];
+}
+
+@end
