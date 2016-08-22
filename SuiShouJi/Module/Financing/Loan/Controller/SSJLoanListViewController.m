7831449be7648a4a3a180d6117770b35df0eebf2
old lang <langxintong@9188.com>
2016-08-22 11:06:58 +0800

    no message

--- SuiShouJi/Module/Financing/Loan/Controller/SSJLoanListViewController.m
+++ SuiShouJi/Module/Financing/Loan/Controller/SSJLoanListViewController.m
@@ -22,6 +22,14 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
 
 @property (nonatomic, strong) UITableView *tableView;
 
+@property (nonatomic, strong) UIImageView *noDataRemindView;
+
+@property (nonatomic, strong) UIView *amountView;
+
+@property (nonatomic, strong) UILabel *amountTitleLab;
+
+@property (nonatomic, strong) UILabel *amountValueLab;
+
 @end
 
 @implementation SSJLoanListViewController
@@ -29,7 +37,7 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
 #pragma mark - Lifecycle
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        
+        self.hidesBottomBarWhenPushed = YES;
     }
     return self;
 }
@@ -43,6 +51,7 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
     self.navigationItem.rightBarButtonItem = addItem;
     
     [self.view addSubview:self.headerSegmentView];
+    [self.view addSubview:self.amountView];
     [self.view addSubview:self.tableView];
     
     [self updateAppearance];
@@ -52,11 +61,35 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
     [super viewWillAppear:animated];
     
     if ([self isMovingToParentViewController]) {
-        
+        // 默认显示未结清的数据，如果没有未结清就显示全部的
+        [self.view ssj_showLoadingIndicator];
+        [SSJLoanHelper queryForLoanModelsWithFundID:_item.fundingID colseOutState:0 success:^(NSArray<SSJLoanModel *> * _Nonnull list) {
+            if (list.count == 0) {
+                [SSJLoanHelper queryForLoanModelsWithFundID:_item.fundingID colseOutState:2 success:^(NSArray<SSJLoanModel *> * _Nonnull list) {
+                    [self.view ssj_hideLoadingIndicator];
+                    self.list = list;
+                    [self.tableView reloadData];
+                    if (list.count == 0) {
+                        [self.view ssj_showWatermarkWithImageName:@"" animated:YES target:nil action:NULL];
+                    } else {
+                        [self.view ssj_hideWatermark:YES];
+                    }
+                } failure:^(NSError * _Nonnull error) {
+                    [self.view ssj_hideLoadingIndicator];
+                    [CDAutoHideMessageHUD showMessage:[error localizedDescription]];
+                }];
+            } else {
+                [self.view ssj_hideLoadingIndicator];
+                self.list = list;
+                [self.tableView reloadData];
+            }
+        } failure:^(NSError * _Nonnull error) {
+            [self.view ssj_hideLoadingIndicator];
+            [CDAutoHideMessageHUD showMessage:[error localizedDescription]];
+        }];
     } else {
-        
+        [self reloadDataAccordingToHeaderViewIndex];
     }
-//    [SSJLoanHelper queryForLoanModelsWithFundID:_item.fundingID colseOutState:<#(int)#> success:<#^(NSArray<SSJLoanModel *> * _Nonnull list)success#> failure:<#^(NSError * _Nonnull error)failure#>]
 }
 
 #pragma mark - UITableViewDataSource
@@ -78,7 +111,7 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
 
 #pragma mark - SCYSlidePagingHeaderViewDelegate
 - (void)slidePagingHeaderView:(SCYSlidePagingHeaderView *)headerView didSelectButtonAtIndex:(NSUInteger)index {
-    
+    [self reloadDataAccordingToHeaderViewIndex];
 }
 
 #pragma mark - Event
@@ -97,6 +130,18 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
     CGFloat alpha = [[SSJThemeSetting currentThemeModel].ID isEqualToString:SSJDefaultThemeID] ? 1 : 0.1;
     _tableView.backgroundColor = [UIColor ssj_colorWithHex:@"#FFFFFF" alpha:alpha];
     _tableView.separatorColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha];
+}
+
+- (void)reloadDataAccordingToHeaderViewIndex {
+    [self.view ssj_showLoadingIndicator];
+    [SSJLoanHelper queryForLoanModelsWithFundID:_item.fundingID colseOutState:(int)_headerSegmentView.selectedIndex success:^(NSArray<SSJLoanModel *> * _Nonnull list) {
+        [self.view ssj_hideLoadingIndicator];
+        self.list = list;
+        [self.tableView reloadData];
+    } failure:^(NSError * _Nonnull error) {
+        [self.view ssj_hideLoadingIndicator];
+        [CDAutoHideMessageHUD showMessage:[error localizedDescription]];
+    }];
 }
 
 #pragma mark - Getter
@@ -125,6 +170,40 @@ static NSString *const kLoanListCellId = @"kLoanListCellId";
         _tableView.rowHeight = 90;
     }
     return _tableView;
+}
+
+- (UIView *)amountView {
+    if (!_amountView) {
+        _amountView = [[UIView alloc] initWithFrame:CGRectMake(0, self.headerSegmentView.bottom, self.view.width, 40)];
+        [_amountView addSubview:self.amountTitleLab];
+        [_amountView addSubview:self.amountValueLab];
+        self.amountTitleLab.left = 19;
+        self.amountValueLab.right = _amountView.width - 19;
+        self.amountTitleLab.centerY = self.amountValueLab.centerY = _amountView.height * 0.5;
+    }
+    return _amountView;
+}
+
+- (UILabel *)amountTitleLab {
+    if (!_amountTitleLab) {
+        _amountTitleLab = [[UILabel alloc] init];
+        _amountTitleLab.font = [UIFont systemFontOfSize:14];
+        if ([_item.fundingParent isEqualToString:@"10"]) {
+            _amountTitleLab.text = @"累计借出款";
+        } else if ([_item.fundingParent isEqualToString:@"11"]) {
+            _amountTitleLab.text = @"累计欠款";
+        }
+        [_amountTitleLab sizeToFit];
+    }
+    return _amountTitleLab;
+}
+
+- (UILabel *)amountValueLab {
+    if (!_amountValueLab) {
+        _amountValueLab = [[UILabel alloc] init];
+        _amountValueLab.font = [UIFont systemFontOfSize:14];
+    }
+    return _amountValueLab;
 }
 
 @end
