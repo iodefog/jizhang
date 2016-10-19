4aaf36be28d2d4e096bd4091d132adb84cfd701d
ricky <125037896@qq.com>
2016-10-19 11:59:53 +0800

    首页导航栏颜色修改

--- SuiShouJi/Module/BookKeeping/BookKeepingHome/View/SSJBookKeepingHomeBar.m
+++ SuiShouJi/Module/BookKeeping/BookKeepingHome/View/SSJBookKeepingHomeBar.m
@@ -14,7 +14,7 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        self.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.naviBarBackgroundColor alpha:SSJ_CURRENT_THEME.backgroundAlpha];
+        self.backgroundColor = [UIColor clearColor];
         [self addSubview:self.budgetButton];
         [self addSubview:self.leftButton];
         [self addSubview:self.rightBarButton];
