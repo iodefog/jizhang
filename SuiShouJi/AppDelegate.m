bcfa350a2f770930789a96ca6bfe004398a5346c
old lang <langxintong@9188.com>
2016-10-25 14:11:46 +0800

    no message

--- SuiShouJi/AppDelegate.m
+++ SuiShouJi/AppDelegate.m
@@ -124,7 +124,10 @@ NSDate *SCYEnterBackgroundTime() {
         [[NSUserDefaults standardUserDefaults]setObject:[NSDate date]forKey:SSJLastPopTimeKey];
         [[NSUserDefaults standardUserDefaults]setBool:NO forKey:SSJHaveLoginOrRegistKey];
         [SSJJspatchAnalyze removePatch];
-        [SSJThemeUpdate UpdateLocalThemesIfneeded];
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [SSJThemeUpdate UpdateLocalThemesIfneeded];
+        });
+        
     }
     
     //每次启动打一次补丁
