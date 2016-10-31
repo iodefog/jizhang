a4955f5bfa308d778baa91067c3c8da5027ff22b
old lang <langxintong@9188.com>
2016-10-31 13:33:57 +0800

    修复数据库bug

--- SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion9.m
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion9.m
@@ -66,9 +66,63 @@
     return nil;
 }
 
+
+/**
+ 将历时预算全部改成总预算，当前预算中类别最多的作为总预算，其他作为分预算；这样处理是因为之前数据库升级有bug，导致app升级到1.8但是数据库没有做相应的版本更新操作，只能采用此方法作为补救措施
+ */
 + (NSError *)updateBudgetTableWithDatabase:(FMDatabase *)db {
-    if (![db executeUpdate:@"UPDATE BK_USER_BUDGET SET CBILLTYPE = 'all'"]) {
+    if (![db executeUpdate:@"UPDATE BK_USER_BUDGET SET CBILLTYPE = 'all' WHERE CEDATE < date('now', 'localtime')"]) {
         return [db lastError];
+    }
+    
+    NSString *weekBudgetId = nil;
+    NSString *monthBudgetId = nil;
+    NSString *yearBudgetId = nil;
+    
+    NSUInteger weekBudgetTypeCount = 0;
+    NSUInteger monthBudgetTypeCount = 0;
+    NSUInteger yearBudgetTypeCount = 0;
+    
+    NSMutableArray *budgetIDs = [NSMutableArray array];
+    
+    FMResultSet *resultSet = [db executeQuery:@"SELECT IBID, ITYPE, CBILLTYPE FROM BK_USER_BUDGET WHERE CEDATE >= DATE('NOW', 'LOCALTIME') AND CSDATE <= DATE('NOW', 'LOCALTIME')"];
+    while ([resultSet next]) {
+        NSString *budgetId = [resultSet stringForColumn:@"IBID"];
+        int type = [resultSet intForColumn:@"ITYPE"];
+        NSString *billType = [resultSet stringForColumn:@"CBILLTYPE"];
+        NSArray *billTypes = [billType componentsSeparatedByString:@","];
+        
+        if (type == 0) {
+            weekBudgetId = weekBudgetTypeCount > billTypes.count ? weekBudgetId : budgetId;
+            weekBudgetTypeCount = billTypes.count;
+        } else if (type == 1) {
+            monthBudgetId = monthBudgetTypeCount > billTypes.count ? monthBudgetId : budgetId;
+            monthBudgetTypeCount = billTypes.count;
+        } else if (type == 2) {
+            yearBudgetId = yearBudgetTypeCount > billTypes.count ? yearBudgetId : budgetId;
+            yearBudgetTypeCount = billTypes.count;
+        }
+    }
+    [resultSet close];
+    
+    
+    if (weekBudgetId) {
+        [budgetIDs addObject:[NSString stringWithFormat:@"'%@'", weekBudgetId]];
+    }
+    
+    if (monthBudgetId) {
+        [budgetIDs addObject:[NSString stringWithFormat:@"'%@'", monthBudgetId]];
+    }
+    
+    if (yearBudgetId) {
+        [budgetIDs addObject:[NSString stringWithFormat:@"'%@'", yearBudgetId]];
+    }
+    
+    if (budgetIDs.count > 0) {
+        NSString *sql = [NSString stringWithFormat:@"UPDATE BK_USER_BUDGET SET CBILLTYPE = 'all' WHERE IBID IN (%@)", [budgetIDs componentsJoinedByString:@","]];
+        if (![db executeUpdate:sql]) {
+            return [db lastError];
+        }
     }
     
     return nil;
