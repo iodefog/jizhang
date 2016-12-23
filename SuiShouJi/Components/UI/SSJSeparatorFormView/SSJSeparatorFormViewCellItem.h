26b3e9ae44790ffa386896f54b77d280c0bdf22d
old lang <langxintong@9188.com>
2016-12-23 09:45:41 +0800

    no message

---
+++ SuiShouJi/Components/UI/SSJSeparatorFormView/SSJSeparatorFormViewCellItem.h
@@ -0,0 +1,13 @@
+//
+//  SSJSeparatorFormViewCellItem.h
+//  SuiShouJi
+//
+//  Created by old lang on 16/12/22.
+//  Copyright © 2016年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSJSeparatorFormViewCellItem : NSObject
+
+@end
