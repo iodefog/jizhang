e8939aea997e4fd4c1870b898d89eb12e4a33c59
old lang <langxintong@9188.com>
2017-01-09 15:13:49 +0800

    no message

--- SuiShouJi/Module/Base/Network/SSJGlobalServiceManager.m
+++ SuiShouJi/Module/Base/Network/SSJGlobalServiceManager.m
@@ -87,14 +87,14 @@
     
     // allowInvalidCertificates 是否允许无效证书（也就是自建的证书），默认为NO
     // 如果是需要验证自建证书，需要设置为YES
-//    securityPolicy.allowInvalidCertificates = _allowInvalidCertificates;
+    securityPolicy.allowInvalidCertificates = _allowInvalidCertificates;
     
     //validatesDomainName 是否需要验证域名，默认为YES；
     //假如证书的域名与你请求的域名不一致，需把该项设置为NO；如设成NO的话，即服务器使用其他可信任机构颁发的证书，也可以建立连接，这个非常危险，建议打开。
     //置为NO，主要用于这种情况：客户端请求的是子域名，而证书上的是另外一个域名。因为SSL证书上的域名是独立的，假如证书上注册的域名是www.google.com，那么mail.google.com是无法验证通过的；当然，有钱可以注册通配符的域名*.google.com，但这个还是比较贵的。
     //如置为NO，建议自己添加对应域名的校验逻辑。
     securityPolicy.validatesDomainName = self.validatesDomainName;
-//    securityPolicy.validatesCertificateChain = NO; // 不用验证整个服务器的证书串，因为目前服务器包含客户端没有的证书
+    securityPolicy.validatesCertificateChain = NO; // 不用验证整个服务器的证书串，因为目前服务器包含客户端没有的证书
     
     return securityPolicy;
 }
@@ -115,7 +115,7 @@
 
 - (void)setAllowInvalidCertificates:(BOOL)allowInvalidCertificates {
     _allowInvalidCertificates = allowInvalidCertificates;
-//    self.securityPolicy.allowInvalidCertificates = allowInvalidCertificates;
+    self.securityPolicy.allowInvalidCertificates = allowInvalidCertificates;
 }
 
 - (void)setValidatesDomainName:(BOOL)validatesDomainName {
