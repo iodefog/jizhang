61d581d488e011993a045c3aedbe846b180e5e24
ricky <125037896@qq.com>
2017-01-09 13:31:57 +0800

    pod修复

---
+++ Pods/Target Support Files/Pods-JiZhangGuanJia/Pods-JiZhangGuanJia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JiZhangGuanJia : NSObject
+@end
+@implementation PodsDummy_Pods_JiZhangGuanJia
+@end
---
+++ Pods/Target Support Files/Pods-YouYuJiZhangBen/Pods-YouYuJiZhangBen-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YouYuJiZhangBen : NSObject
+@end
+@implementation PodsDummy_Pods_YouYuJiZhangBen
+@end
