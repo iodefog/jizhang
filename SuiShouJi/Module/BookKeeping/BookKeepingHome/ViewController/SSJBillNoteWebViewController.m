5efe669d95c7cb76a9e2f1a0e9a335d13263abc3
ricky <125037896@qq.com>
2017-01-16 13:41:21 +0800

    bug fixed

--- SuiShouJi/Module/BookKeeping/BookKeepingHome/ViewController/SSJBillNoteWebViewController.m
+++ SuiShouJi/Module/BookKeeping/BookKeepingHome/ViewController/SSJBillNoteWebViewController.m
@@ -99,6 +99,9 @@
 - (void)backButtonClicked
 {
     [self dismissViewControllerAnimated:YES completion:nil];
+    if (self.backButtonClickBlock) {
+        self.backButtonClickBlock();
+    }
 }
 
 #pragma mark - Private
