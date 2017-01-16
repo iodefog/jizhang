fda79e77154a6ac1c6a78ce15cabf9d142949bbe
ricky <125037896@qq.com>
2017-01-16 11:05:22 +0800

    tableview截图

--- SuiShouJi/Additons/UIKit/SSJViewAddition.m
+++ SuiShouJi/Additons/UIKit/SSJViewAddition.m
@@ -547,5 +547,12 @@ static const void *kShowViewsIdentifier = &kShowViewsIdentifier;
     return [self.layer ssj_takeScreenShotWithSize:size opaque:opaque scale:scale];
 }
 
+- (UIImage *)ssj_takeScreenShotForTableView {
+    if (![self isKindOfClass:[UITableView class]]) {
+        return nil;
+    }
+    return nil;
+}
+
 @end
 
