ce40e500f8b1fdc82eaaa7a8aaac4be9ee867a32
ricky <125037896@qq.com>
2017-02-10 13:49:53 +0800

    数据库升级操作

--- SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion12.m
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion12.m
@@ -21,12 +21,18 @@
 
 + (NSError *)updateUserChargeTableWithDatabase:(FMDatabase *)db {
 
+    // 添加记账时分字段
     if (![db executeUpdate:@"alter table bk_user_charge add cdetaildate text"]) {
         return [db lastError];
     }
 
-    if (![db executeUpdate:@"update bk_user_charge set "]) {
-        
+    // 修改记账时分字段
+    if (![db executeUpdate:@"update bk_user_charge set cdetaildate = (select substr(clientadddate,12,5) from bk_user_charge) from bk_user_charge where length(clientadddate) > 0"]) {
+        return [db lastError];
+    }
+    
+    if (![db executeUpdate:@"update bk_user_charge set cdetaildate = (select substr(cwritedate,12,5) from bk_user_charge) from bk_user_charge where length(cdetaildate) = 0 or cdetaildate is null"]) {
+        return [db lastError];
     }
     
     return nil;
