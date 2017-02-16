c2930e8562b2c6153c33c5e63df744ae059cbe5b
old lang <langxintong@9188.com>
2017-02-16 20:35:49 +0800

    配置生产、测试环境

--- SuiShouJi/Components/Business/SSJDomainManager/SSJDomainManager.m
+++ SuiShouJi/Components/Business/SSJDomainManager/SSJDomainManager.m
@@ -23,8 +23,6 @@ static NSString *const kSSJDomainKey = @"SSJDomainManagerKey";
 @implementation SSJDomainManager
 
 + (NSString *)domain {
-    return [self formalDomain];
-    
 #ifdef PRODUCTION
     return [self formalDomain];
 #else
@@ -33,8 +31,6 @@ static NSString *const kSSJDomainKey = @"SSJDomainManagerKey";
 }
 
 + (NSString *)imageDomain {
-    return [self formalDomain];
-
 #ifdef PRODUCTION
     return [self formalDomain];
 #else
