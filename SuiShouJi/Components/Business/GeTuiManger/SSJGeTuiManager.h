ed168020b85ecf65f9261cfada2e76577dc09f24
ricky <125037896@qq.com>
2017-02-22 18:02:53 +0800

    增加个推sdk

---
+++ SuiShouJi/Components/Business/GeTuiManger/SSJGeTuiManager.h
@@ -0,0 +1,13 @@
+//
+//  SSJGeTuiManager.h
+//  SuiShouJi
+//
+//  Created by ricky on 2017/2/22.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSJGeTuiManager : NSObject
+
+@end
---
+++ SuiShouJi/Components/Business/GeTuiManger/SSJGeTuiManager.m
@@ -0,0 +1,13 @@
+//
+//  SSJGeTuiManager.m
+//  SuiShouJi
+//
+//  Created by ricky on 2017/2/22.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJGeTuiManager.h"
+
+@implementation SSJGeTuiManager
+
+@end
