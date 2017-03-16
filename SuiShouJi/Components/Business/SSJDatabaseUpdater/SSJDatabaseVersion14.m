3a7108cf7c050902dbc723e0f8e4a71f71b77d50
ricky <125037896@qq.com>
2017-03-16 11:31:15 +0800

    更新个推sdk，资金类型增加其他类型，修改target

---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion14.m
@@ -0,0 +1,36 @@
+//
+//  SSJDatabaseVersion14.m
+//  SuiShouJi
+//
+//  Created by ricky on 2017/3/16.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJDatabaseVersion14.h"
+#import "SSJDatabaseQueue.h"
+
+@implementation SSJDatabaseVersion14
+
++ (NSError *)startUpgradeInDatabase:(FMDatabase *)db {
+    
+    NSError *error = [self updateFundInfoTableWithDatabase:db];
+    if (error) {
+        return error;
+    }
+    
+    return nil;
+}
+
++ (NSError *)updateFundInfoTableWithDatabase:(FMDatabase *)db {
+    
+    if (![db intForQuery:@"select count(1) from bk_fund_info where cfundid = '15' and cparent = 'root'"]) {
+        if (![db executeUpdate:@"insert into bk_fund_info (cfundid, cacctname, cicoin, cparent, cwritedate, operatortype) values ('15', '其他', 'ft_others', 'root', '-1', 0)"]) {
+            return [db lastError];
+        }
+    }
+    
+    return nil;
+}
+
+
+@end
