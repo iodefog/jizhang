5a660bc40cdad78b6909bd7abefdfa3559ac591f
mzl <2736652044@qq.com>
2017-05-02 15:28:32 +0800

    背景

--- SuiShouJi/Module/BookKeeping/BookKeepingHome/View/SSJThemBgImageClipViewController.m
+++ SuiShouJi/Module/BookKeeping/BookKeepingHome/View/SSJThemBgImageClipViewController.m
@@ -21,9 +21,14 @@
 
 /**<#注释#>*/
 @property (nonatomic, strong) UIImage *selectedImage;
+@property (nonatomic, strong) UIImage *oldImage;
+@property (nonatomic, strong) CALayer *clipView;
+/**<#注释#>*/
+@property (nonatomic, assign) CGSize oldImagesize;
 
+@property (nonatomic, strong) CAShapeLayer *coverLayer;
 @end
-
+static CGFloat imageScale = 0.8; //裁剪框和屏幕大小比例
 @implementation SSJThemBgImageClipViewController
 
 - (void)viewDidLoad
@@ -35,6 +40,8 @@
     [self.view addSubview:self.chooseButton];
     self.view.backgroundColor = [UIColor blackColor];
     self.scrollview.backgroundColor = [UIColor blackColor];
+    [self.view.layer addSublayer:self.coverLayer];
+    [self.view.layer addSublayer:self.clipView];
 }
 
 - (void)viewWillLayoutSubviews
@@ -43,6 +50,11 @@
     self.scrollview.frame = self.view.bounds;
     self.cancelButton.leftBottom = CGPointMake(20, self.view.height - 20);
     self.chooseButton.rightBottom = CGPointMake(self.view.width - 20, self.view.height - 20);
+    self.clipView.size = CGSizeMake(self.view.width*imageScale, self.view.height*imageScale);
+    self.clipView.left = (self.view.width - self.clipView.size.width)*0.5;
+    self.clipView.top = (self.view.height - self.clipView.size.height)*0.5;
+    
+    self.view.frame = self.view.bounds;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -64,16 +76,11 @@
 
 #pragma mark - UIScrollViewDelegate
 - (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
-    [self.imageView sizeToFit];
-    self.scrollview.contentSize = self.imageView.size;
-    self.imageView.center = CGPointMake(self.scrollview.contentSize.width*0.5, self.scrollview.contentSize.height*0.5);
-    if (self.imageView.size.width < self.view.width) {
-        self.imageView.centerX = self.view.width*0.5;
-    }
-    if (self.imageView.size.height<self.view.height) {
-        self.imageView.centerY = self.view.height*0.5;
-    }
+    
 }
 
 - (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
@@ -85,6 +92,8 @@
 - (void)setNormalImage:(UIImage *)normalImage
 {
     _normalImage = normalImage;
+    self.oldImagesize = CGSizeMake(normalImage.size.width, normalImage.size.height);
+    self.oldImage = normalImage;
     self.imageView.image = normalImage;
     self.imageView.size = CGSizeMake(normalImage.size.width, normalImage.size.height);
     self.scrollview.contentSize = self.imageView.size;
@@ -100,11 +109,19 @@
 #pragma mark - Event
 - (void)chooseButtonClicked
 {
-    //裁剪图片
-    //隐藏取消选择按钮
-    self.chooseButton.hidden = YES;
-    self.cancelButton.hidden = YES;
-   self.selectedImage = [self screenView:self.view];
+    //计算裁剪比例
+    CGFloat imageClipScale = self.oldImagesize.width / self.imageView.width;
+    CGSize clipSize = CGSizeMake(self.clipView.width * imageClipScale, self.clipView.height * imageClipScale);
+    CGFloat clipLeft = 0;
+    CGFloat clipTop = 0;
+    
+    CGRect imageViewFrame = [self.imageView convertRect:self.imageView.frame toView:self.view];
+    clipLeft = (self.clipView.left - imageViewFrame.origin.x) * imageClipScale;
+    clipTop = (self.clipView.top - imageViewFrame.origin.y) * imageClipScale;
+    
+    CGRect clipRect = CGRectMake(clipLeft, clipTop, clipSize.width, clipSize.height);
+    self.selectedImage = [self imageFromView:self.view atFrame:clipRect];
+    [UIImagePNGRepresentation(self.selectedImage) writeToFile:@"/Users/yicai/Desktop/test.png" atomically:YES];
     if (!self.selectedImage) return;
     if (self.clipImageBlock) {
         self.clipImageBlock(self.selectedImage);
@@ -127,6 +144,38 @@
     UIGraphicsEndImageContext();
     return img;
 }
+//获得某个范围内的屏幕图像
+- (UIImage *)imageFromView: (UIView *)theView atFrame:(CGRect)rect
+{
+//    UIGraphicsBeginImageContext(r.size);
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGContextSaveGState(context);
+//    UIRectClip(r);
+//    [theView.layer renderInContext:context];
+//    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+//    
+//    UIGraphicsEndImageContext();
+    UIImage * imgeee = [UIImage imageWithCGImage:CGImageCreateWithImageInRect(self.oldImage.CGImage, rect)];
+    return  imgeee;
+}
+
+//返回裁剪区域图片,返回裁剪区域大小图片
+
+- (UIImage *)clipWithImageRect:(CGRect)clipRect clipImage:(UIImage *)clipImage;
+
+{
+    
+    UIGraphicsBeginImageContext(clipRect.size);
+    
+    [clipImage drawInRect:CGRectMake(0,0,clipRect.size.width,clipRect.size.height)];
+    
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return  newImage;
+    
+}
 
 #pragma mark - Lazy
 - (UIScrollView *)scrollview
@@ -136,6 +185,7 @@
         _scrollview.delegate = self;
         _scrollview.minimumZoomScale = 0.2;
         _scrollview.maximumZoomScale = MAXFLOAT;
+        self.scrollview.contentInset = UIEdgeInsetsMake((self.view.height - self.clipView.height) * 0.5, (self.view.width - self.clipView.width) * 0.5, (self.view.height - self.clipView.height) * 0.5, (self.view.width - self.clipView.width) * 0.5);
     }
     return _scrollview;
 }
@@ -168,5 +218,64 @@
         [_cancelButton sizeToFit];
     }
     return _cancelButton;
+}
+
+- (CALayer *)clipView {
+    if (!_clipView) {
+        _clipView = [CALayer layer];
+        _clipView.borderWidth = 1;
+        _clipView.backgroundColor = [UIColor clearColor].CGColor;
+        _clipView.borderColor = [UIColor whiteColor].CGColor;
+    }
+    return _clipView;
+}
+
+- (CAShapeLayer *)coverLayer
+{
+    if (!_coverLayer) {
+        _coverLayer = [CAShapeLayer layer];
+        UIBezierPath *path = [UIBezierPath bezierPath];
+        
+        CGPoint leftTop = CGPointMake(0, 0);
+        CGPoint rightTop = CGPointMake(SSJSCREENWITH, 0);
+        CGPoint leftBottom = CGPointMake(0, SSJSCREENHEIGHT);
+        CGPoint rightBottom = CGPointMake(SSJSCREENWITH, SSJSCREENHEIGHT);
+        
+        CGFloat leftPad = SSJSCREENWITH * (1 - imageScale) * 0.5;
+        CGFloat topPad = SSJSCREENHEIGHT * (1 - imageScale) * 0.5;
+        CGPoint leftTops = CGPointMake(leftPad, SSJSCREENHEIGHT * (1 - imageScale) * 0.5);
+        CGPoint rightTops = CGPointMake(SSJSCREENWITH - leftPad, topPad);
+        CGPoint leftBottoms = CGPointMake(leftPad, SSJSCREENHEIGHT - topPad);
+        CGPoint rightBottoms = CGPointMake(SSJSCREENWITH - leftPad, SSJSCREENHEIGHT - topPad);
+        
+        [path moveToPoint:leftTop];
+        [path addLineToPoint:leftTops];
+        [path addLineToPoint:rightTops];
+        [path addLineToPoint:rightTop];
+        [path closePath];
+        
+        [path moveToPoint:rightTop];
+        [path addLineToPoint:rightTops];
+        [path addLineToPoint:rightBottoms];
+        [path addLineToPoint:rightBottom];
+        [path closePath];
+        
+        [path moveToPoint:rightBottom];
+        [path addLineToPoint:rightBottoms];
+        [path addLineToPoint:leftBottoms];
+        [path addLineToPoint:leftBottom];
+        [path closePath];
+        
+        [path moveToPoint:leftBottom];
+        [path addLineToPoint:leftBottoms];
+        [path addLineToPoint:leftTops];
+        [path addLineToPoint:leftTop];
+        [path closePath];
+        
+        
+        [_coverLayer setFillColor:[UIColor ssj_colorWithHex:@"000000" alpha:0.5].CGColor];
+        _coverLayer.path = path.CGPath;
+    }
+    return _coverLayer;
 }
 @end
