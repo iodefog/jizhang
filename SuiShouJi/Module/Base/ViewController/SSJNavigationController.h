8192b4da847219caf3143d04515b1364376b4d9a
old lang <langxintong@9188.com>
2017-05-04 14:05:13 +0800

    no message

--- SuiShouJi/Module/Base/ViewController/SSJNavigationController.h
+++ SuiShouJi/Module/Base/ViewController/SSJNavigationController.h
@@ -14,12 +14,18 @@
 
 @interface SSJNavigationController : UINavigationController
 
+/**
+ 导航栏控制器的代理，不要设置原有的delegate，用此属性代替；
+ */
 @property (nonatomic, weak) id<SSJNavigationControllerDelegate> customDelegate;
 
 @end
 
 @interface UIViewController (SSJNavigationController)
 
+/**
+ 是否隐藏导航栏；只有设置SSJNavigationController的子控制器有效
+ */
 @property (nonatomic, getter=ssj_hidesNavigationBarWhenPushed, setter=ssj_setHidesNavigationBarWhenPushed:) BOOL hidesNavigationBarWhenPushed;
 
 @end
