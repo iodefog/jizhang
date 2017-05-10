d905b9d6cad39d439784ce261b21e5fd961439c4
old lang <langxintong@9188.com>
2017-05-10 13:43:22 +0800

    no message

---
+++ SuiShouJi/Components/Business/DataSync/OtherTable/SSJUserTable.m
@@ -0,0 +1,13 @@
+//
+//  SSJUserTable.m
+//  SuiShouJi
+//
+//  Created by old lang on 17/5/9.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJUserTable.h"
+
+@implementation SSJUserTable
+
+@end
