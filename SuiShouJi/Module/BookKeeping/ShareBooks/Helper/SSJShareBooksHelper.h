4ad99b7295d67e9c42d19c1c1426c27a382e7d71
ricky <125037896@qq.com>
2017-05-12 16:05:27 +0800

    共享账本目录结构

---
+++ SuiShouJi/Module/BookKeeping/ShareBooks/Helper/SSJShareBooksHelper.h
@@ -0,0 +1,13 @@
+//
+//  SSJShareBooksHelper.h
+//  SuiShouJi
+//
+//  Created by ricky on 2017/5/12.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSJShareBooksHelper : NSObject
+
+@end
---
+++ SuiShouJi/Module/BookKeeping/ShareBooks/Helper/SSJShareBooksHelper.m
@@ -0,0 +1,14 @@
+
+//
+//  SSJShareBooksHelper.m
+//  SuiShouJi
+//
+//  Created by ricky on 2017/5/12.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJShareBooksHelper.h"
+
+@implementation SSJShareBooksHelper
+
+@end
