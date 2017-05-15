2dd0809a006dad3ad6ba0715ca81597a51bd4348
ricky <125037896@qq.com>
2017-05-15 19:45:32 +0800

    成员修改

--- SuiShouJi/Module/BookKeeping/ShareBooks/View/SSJSharebooksMemberCollectionViewCell.m
+++ SuiShouJi/Module/BookKeeping/ShareBooks/View/SSJSharebooksMemberCollectionViewCell.m
@@ -20,4 +20,49 @@
 
 @implementation SSJSharebooksMemberCollectionViewCell
 
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self.contentView addSubview:self.iconImageView];
+        [self.contentView addSubview:self.nickNameLabel];
+        
+    }
+    return self;
+}
+
+- (UIImageView *)iconImageView {
+    if (!_iconImageView) {
+        _iconImageView = [[UIImageView alloc] init];
+        _iconImageView.layer.cornerRadius = self.width / 2;
+        _iconImageView.layer.borderWidth = 1 / [UIScreen mainScreen].scale;
+    }
+    return _iconImageView;
+}
+
+- (UILabel *)nickNameLabel {
+    if (!_nickNameLabel) {
+        _nickNameLabel = [[UILabel alloc] init];
+        _nickNameLabel.font = [UIFont ssj_pingFangRegularFontOfSize:SSJ_FONT_SIZE_4];
+        _nickNameLabel.textColor = [UIColor ssj_colorWithHex:@"#333333"];
+        _nickNameLabel.textAlignment = NSTextAlignmentCenter;
+    }
+    return _nickNameLabel;
+}
+
+- (void)updateConstraints {
+    [self.iconImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.height.mas_equalTo(self.contentView.mas_width);
+        make.left.top.mas_equalTo(0);
+    }];
+    
+    [self.nickNameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.iconImageView.mas_top).offset(10);
+        make.centerX.mas_equalTo(self.contentView.mas_centerX);
+        make.width.mas_equalTo(self.contentView.mas_width);
+    }];
+    
+    [super updateConstraints];
+}
+
 @end
