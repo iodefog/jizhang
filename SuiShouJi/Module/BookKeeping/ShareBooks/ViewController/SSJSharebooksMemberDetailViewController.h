3acc096b1f249f8b4fdd265e9538d9004bd4d569
ricky <125037896@qq.com>
2017-05-19 14:42:16 +0800

    成员详情页面

---
+++ SuiShouJi/Module/BookKeeping/ShareBooks/ViewController/SSJSharebooksMemberDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSJSharebooksMemberDetailViewController.h
+//  SuiShouJi
+//
+//  Created by ricky on 2017/5/19.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJBaseViewController.h"
+
+@interface SSJSharebooksMemberDetailViewController : SSJBaseViewController
+
+@end
