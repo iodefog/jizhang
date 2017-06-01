65215ed01ed974e6dc289c702217cbaf7d9de571
ricky <125037896@qq.com>
2017-06-01 12:01:11 +0800

    账本成员修改

--- SuiShouJi/Components/Business/DataSync/SyncTable/SSJShareBooksMemberSyncTable.m
+++ SuiShouJi/Components/Business/DataSync/SyncTable/SSJShareBooksMemberSyncTable.m
@@ -53,43 +53,7 @@
 }
 
 + (NSArray *)queryRecordsNeedToSyncWithUserId:(NSString *)userId inDatabase:(FMDatabase *)db error:(NSError **)error {
-    int64_t version = [SSJSyncTable lastSuccessSyncVersionForUserId:userId inDatabase:db];
-    if (version == SSJ_INVALID_SYNC_VERSION) {
-        if (error) {
-            *error = [db lastError];
-        }
-        return nil;
-    }
-    
-    FMResultSet *result = [db executeQuery:@"select * from bk_share_books where cbooksid in (select cbooksid from bk_share_books_member where cmemberid = ?)", userId];
-    if (!result) {
-        if (error) {
-            *error = [db lastError];
-        }
-        return nil;
-    }
-    
     NSMutableArray *syncRecords = [NSMutableArray array];
-    while ([result next]) {
-        NSString *ID = [result stringForColumn:@"cbooksid"];
-        NSString *ccreator = [result stringForColumn:@"ccreator"];
-        NSString *cadmin = [result stringForColumn:@"cadmin"];
-        NSString *cbooksname = [result stringForColumn:@"cbooksname"];
-        NSString *cbookscolor = [result stringForColumn:@"cbookscolor"];
-        NSInteger iparenttype = [result intForColumn:@"iparenttype"];
-        NSString *iversion = [result stringForColumn:@"iversion"];
-        NSString *cwritedate = [result stringForColumn:@"cwritedate"];
-        NSInteger operatortype = [result intForColumn:@"operatortype"];
-        [syncRecords addObject:@{@"cbooksid":ID,
-                                 @"ccreator":ccreator,
-                                 @"cadmin":cadmin,
-                                 @"cbooksname":cbooksname,
-                                 @"iparenttype":@(iparenttype),
-                                 @"cbookscolor":cbookscolor,
-                                 @"iversion":iversion,
-                                 @"cwritedate":cwritedate,
-                                 @"operatortype":@(operatortype)}];
-    }
     return syncRecords;
 }
 
