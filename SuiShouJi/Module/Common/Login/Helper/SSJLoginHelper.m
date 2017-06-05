030f267b74e851badf31c16b7f410dcc712e72b5
old lang <langxintong@9188.com>
2017-06-05 16:10:44 +0800

    修复登录后更新父类型为空的账本逻辑错误 导致数据库操作失败

--- SuiShouJi/Module/Common/Login/Helper/SSJLoginHelper.m
+++ SuiShouJi/Module/Common/Login/Helper/SSJLoginHelper.m
@@ -27,7 +27,7 @@
 
 + (void)updateBooksParentIfNeededForUserId:(NSString *)userId inDatabase:(FMDatabase *)db error:(NSError **)error {
     // 更新默认账本的记账类型
-    if (![db executeUpdate:@"update bk_books_type set iparenttype = case when length(cbooksid) != length(cuserid) and cbooksid like cuserid || '%' then substr(cbooksid, length(cuserid) + 2, length(cbooksid) - length(cuserid) - 1) when cbooksid = cuserid then '0' end ,iversion = ? ,cwritedate = ?",@(SSJSyncVersion()),[[NSDate date] formattedDateWithFormat:@"yyyy-MM-dd HH:mm:ss.SSS"]]) {
+    if (![db executeUpdate:@"update bk_books_type set iparenttype = case when length(cbooksid) != length(cuserid) and cbooksid like cuserid || '%' then substr(cbooksid, length(cuserid) + 2, length(cbooksid) - length(cuserid) - 1) when cbooksid = cuserid then '0' end ,iversion = ? ,cwritedate = ? where iparenttype is null",@(SSJSyncVersion()),[[NSDate date] formattedDateWithFormat:@"yyyy-MM-dd HH:mm:ss.SSS"]]) {
         if (error) {
             *error = [db lastError];
         }
