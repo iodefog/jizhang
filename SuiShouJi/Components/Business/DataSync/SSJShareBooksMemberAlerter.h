2857879caf8f53dcf3f6c53093c48293ec191773
old lang <langxintong@9188.com>
2017-06-08 11:15:38 +0800

    share_books_member 增加CLEAVEDATE字段

---
+++ SuiShouJi/Components/Business/DataSync/SSJShareBooksMemberAlerter.h
@@ -0,0 +1,13 @@
+//
+//  SSJShareBooksMemberAlerter.h
+//  SuiShouJi
+//
+//  Created by old lang on 17/6/8.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSJShareBooksMemberAlerter : NSObject
+
+@end
