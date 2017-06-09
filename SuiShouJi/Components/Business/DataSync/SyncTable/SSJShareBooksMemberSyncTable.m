f8e5fe798713a07add0dddae3d3da3f66633fe4a
old lang <langxintong@9188.com>
2017-06-09 16:10:44 +0800

    修复“share_books_member表同步遗漏color、cleavedate两个字段”

--- SuiShouJi/Components/Business/DataSync/SyncTable/SSJShareBooksMemberSyncTable.m
+++ SuiShouJi/Components/Business/DataSync/SyncTable/SSJShareBooksMemberSyncTable.m
@@ -15,33 +15,20 @@
     return @"bk_share_books_member";
 }
 
-+ (NSArray *)columns {
-    return @[@"cmemberid",
-             @"cbooksid",
-             @"cjoindate",
-             @"istate",
-             @"cicon",
-             @"ccolor"];
-}
-
-+ (NSArray *)primaryKeys {
-    return @[@"cmemberid",@"cbooksid"];
-}
-
 + (BOOL)mergeRecords:(NSArray *)records forUserId:(NSString *)userId inDatabase:(FMDatabase *)db error:(NSError **)error {
     for (NSDictionary *recordInfo in records) {
         BOOL exist = [db boolForQuery:@"select count(*) from bk_share_books_member where cmemberid = ? and cbooksid = ?", recordInfo[@"cmemberid"], recordInfo[@"cbooksid"]];
 
         
         if (exist) {
-            if (![db executeUpdate:@"update bk_share_books_member set cjoindate = ? ,istate = ? ,cicon = ? where cbooksid = ? and cmemberid = ?", recordInfo[@"cjoindate"], recordInfo[@"istate"], recordInfo[@"cicon"], recordInfo[@"cbooksid"], recordInfo[@"cmemberid"]]) {
+            if (![db executeUpdate:@"update bk_share_books_member set cjoindate = ? ,istate = ? ,cicon = ?, ccolor = ?, cleavedate = ? where cbooksid = ? and cmemberid = ?", recordInfo[@"cjoindate"], recordInfo[@"istate"], recordInfo[@"cicon"], recordInfo[@"ccolor"], recordInfo[@"cleavedate"], recordInfo[@"cbooksid"], recordInfo[@"cmemberid"]]) {
                 if (error) {
                     *error = [db lastError];
                 }
                 return NO;
             }
         } else {
-            if (![db executeUpdate:@"insert into bk_share_books_member (cjoindate,istate,cicon,cbooksid,cmemberid) values (?,?,?,?,?)", recordInfo[@"cjoindate"], recordInfo[@"istate"], recordInfo[@"cicon"], recordInfo[@"cbooksid"], recordInfo[@"cmemberid"]]) {
+            if (![db executeUpdate:@"insert into bk_share_books_member (cjoindate, istate, cicon, ccolor, cleavedate, cbooksid, cmemberid) values (?, ?, ?, ?, ?)", recordInfo[@"cjoindate"], recordInfo[@"istate"], recordInfo[@"cicon"], recordInfo[@"ccolor"], recordInfo[@"cleavedate"], recordInfo[@"cbooksid"], recordInfo[@"cmemberid"]]) {
                 if (error) {
                     *error = [db lastError];
                 }
