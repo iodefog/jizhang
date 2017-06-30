5fa7bc4ff2b00000bff01e180d8eca366ecf12dc
old lang <langxintong@9188.com>
2017-06-30 13:49:32 +0800

    优化代码

--- SuiShouJi/Module/Mine/BindMobileNo/Controller/SSJBindMobileNoViewController.m
+++ SuiShouJi/Module/Mine/BindMobileNo/Controller/SSJBindMobileNoViewController.m
@@ -107,7 +107,7 @@
 
     RAC(self.viewModel, phoneNum) = self.phoneNoField.rac_textSignal;
     self.nextBtn.rac_command = self.viewModel.verifyPhoneNumRequestCommand;
-    [self.viewModel.verifyPhoneNumRequestCommand.executionSignals.switchToLatest subscribeNext:^(NSNumber *result) {
+    [self.nextBtn.rac_command.executionSignals.switchToLatest subscribeNext:^(NSNumber *result) {
         @strongify(self);
         if ([result boolValue]) {
             NSError *error = [NSError errorWithDomain:SSJErrorDomain code:SSJErrorCodeUndefined userInfo:@{NSLocalizedDescriptionKey:@"此手机号已经注册过了，换一个吧"}];
