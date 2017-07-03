ca5a55dc0586d5544504c18970ec1783f4d2775a
old lang <langxintong@9188.com>
2017-07-03 15:54:09 +0800

    所有数据全部同步

--- SuiShouJi/Module/Mine/MoreHome/Helper/SSJDataClearHelper.h
+++ SuiShouJi/Module/Mine/MoreHome/Helper/SSJDataClearHelper.h
@@ -16,4 +16,14 @@
 
 + (void)clearAllDataWithSuccess:(void(^)())success
                         failure:(void (^)(NSError *error))failure;
+
+/**
+ 上传当前用户所有数据
+
+ @param success 成功回调；syncTime代表同步成功的时间
+ @param failure 失败回调
+ */
++ (void)uploadAllUserDataWithSuccess:(void(^)(NSString *syncTime))success
+                             failure:(void (^)(NSError *error))failure;
+
 @end
