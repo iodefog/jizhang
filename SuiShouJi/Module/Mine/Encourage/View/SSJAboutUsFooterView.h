89b5cc470385a99e00ac57434949d7bdbaa6cff2
ricky <125037896@qq.com>
2017-07-05 16:25:31 +0800

    添加团队协议和用户协议

---
+++ SuiShouJi/Module/Mine/Encourage/View/SSJAboutUsFooterView.h
@@ -0,0 +1,16 @@
+//
+//  SSJAboutUsFooterView.h
+//  SuiShouJi
+//
+//  Created by ricky on 2017/7/5.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSJAboutUsFooterView : UIView
+
+@property (nonatomic, copy) void (^buttonClickBlock)(NSString *title);
+
+
+@end
