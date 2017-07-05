6ca187f18754d183020e9f11c20293cc13ddb08d
mzl <2736652044@qq.com>
2017-07-05 16:28:00 +0800

    记账反馈

---
+++ SuiShouJi/Module/Mine/MoreHome/ViewController/SSJHelpAndAdviceViewController.h
@@ -0,0 +1,13 @@
+//
+//  SSJHelpAndAdviceViewController.h
+//  SuiShouJi
+//
+//  Created by yi cai on 2017/7/5.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJAdWebViewController.h"
+
+@interface SSJHelpAndAdviceViewController : SSJBaseViewController
+
+@end
---
+++ SuiShouJi/Module/Mine/MoreHome/ViewController/SSJHelpAndAdviceViewController.m
@@ -0,0 +1,87 @@
+//
+//  SSJHelpAndAdviceViewController.m
+//  SuiShouJi
+//
+//  Created by yi cai on 2017/7/5.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJHelpAndAdviceViewController.h"
+#import "SSJProductAdviceViewController.h"
+
+@interface SSJHelpAndAdviceViewController ()<UIWebViewDelegate>
+
+@property (nonatomic, strong) UIButton *helpBtn;
+
+@property (nonatomic, strong) UIWebView *webView;
+@end
+
+@implementation SSJHelpAndAdviceViewController
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        self.hidesBottomBarWhenPushed = YES;
+        self.appliesTheme = NO;
+    }
+    return self;
+}
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view addSubview:self.webView];
+    [self.view addSubview:self.helpBtn];
+    [self updateViewConstraints];
+}
+
+- (void)updateViewConstraints {
+    [self.webView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.mas_offset(0);
+        make.top.mas_equalTo(SSJ_NAVIBAR_BOTTOM);
+        make.bottom.mas_offset(-50);
+    }];
+    
+    [self.helpBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.bottom.mas_offset(0);
+        make.top.mas_equalTo(self.webView.mas_bottom);
+        make.height.mas_equalTo(50);
+    }];
+    [super updateViewConstraints];
+}
+
+#pragma mark - UIWebViewDelegate
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    [CDAutoHideMessageHUD showMessage:error.description];
+}
+
+- (UIButton *)helpBtn {
+    if (!_helpBtn) {
+        _helpBtn = [[UIButton alloc] init];
+        [_helpBtn setTitle:@"反馈" forState:UIControlStateNormal];
+        [_helpBtn setTitleColor:[UIColor ssj_colorWithHex:[SSJThemeSetting defaultThemeModel].marcatoColor] forState:UIControlStateNormal];
+        _helpBtn.titleLabel.font = [UIFont ssj_pingFangRegularFontOfSize:SSJ_FONT_SIZE_2];
+        @weakify(self);
+        [[_helpBtn rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(id x) {
+            @strongify(self);
+            SSJProductAdviceViewController *proVC = [[SSJProductAdviceViewController alloc] init];
+            [self.navigationController pushViewController:proVC animated:YES];
+        }];
+        [_helpBtn ssj_setBorderColor:[UIColor ssj_colorWithHex:[SSJThemeSetting defaultThemeModel].cellSeparatorColor]];
+        [_helpBtn ssj_setBorderStyle:SSJBorderStyleTop];
+        [_helpBtn ssj_setBorderWidth:1];
+        _helpBtn.backgroundColor = [UIColor whiteColor];
+    }
+    return _helpBtn;
+}
+
+- (UIWebView *)webView {
+    if (!_webView) {
+        _webView = [[UIWebView alloc] init];
+        NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://jzcms.youyuwo.com/a/bangzhu/index.html"]];
+        [_webView loadRequest:request];
+        _webView.delegate = self;
+    }
+    return _webView;
+}
+
+
+@end
