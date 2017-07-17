6c1b748a4dc4876bdf3e4fc613dbdd743ac30295
mzl <2736652044@qq.com>
2017-07-17 16:04:10 +0800

    许下心愿页面UI搭建

---
+++ SuiShouJi/Module/Mine/MoreHome/View/SSJMakeWishMoneyCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  SSJMakeWishMoneyCollectionViewCell.h
+//  SuiShouJi
+//
+//  Created by yi cai on 2017/7/17.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSJMakeWishMoneyCollectionViewCell : UICollectionViewCell
+/**money*/
+@property (nonatomic, copy) NSString *amontStr;
+
+@end
---
+++ SuiShouJi/Module/Mine/MoreHome/View/SSJMakeWishMoneyCollectionViewCell.m
@@ -0,0 +1,68 @@
+//
+//  SSJMakeWishMoneyCollectionViewCell.m
+//  SuiShouJi
+//
+//  Created by yi cai on 2017/7/17.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJMakeWishMoneyCollectionViewCell.h"
+
+@interface SSJMakeWishMoneyCollectionViewCell ()
+
+@property (nonatomic, strong) UILabel *moneyBtn;
+@end
+
+@implementation SSJMakeWishMoneyCollectionViewCell
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        [self.contentView addSubview:self.moneyBtn];
+        
+        [self updateConstraintsIfNeeded];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateThemeAfterThemeChanged) name:SSJThemeDidChangeNotification object:nil];
+        [self updateThemeAfterThemeChanged];
+    }
+    return self;
+}
+
+- (void)updateThemeAfterThemeChanged {
+    UIImage *selectedImage = [UIImage ssj_imageWithColor:[UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha] size:CGSizeZero];
+    self.selectedBackgroundView = [[UIImageView alloc] initWithImage:selectedImage];
+    
+    self.moneyBtn.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor];
+    self.moneyBtn.layer.borderColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha].CGColor;
+}
+
+
+- (UILabel *)moneyBtn {
+    if (!_moneyBtn) {
+        _moneyBtn = [[UILabel alloc] init];
+        _moneyBtn.font = [UIFont ssj_pingFangRegularFontOfSize:SSJ_FONT_SIZE_4];
+        _moneyBtn.textAlignment = NSTextAlignmentCenter;
+        _moneyBtn.layer.borderWidth = 1/SSJSCREENSCALE;
+        _moneyBtn.layer.cornerRadius = 6;
+        _moneyBtn.layer.masksToBounds = YES;
+        _moneyBtn.backgroundColor = [UIColor clearColor];
+    }
+    return _moneyBtn;
+}
+
+- (void)updateConstraints {
+    
+    [self.moneyBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.bottom.left.right.mas_equalTo(0);
+    }];
+    [super updateConstraints];
+}
+
+- (void)setAmontStr:(NSString *)amontStr {
+    _amontStr = amontStr;
+    _moneyBtn.text = amontStr;
+}
+
+@end
