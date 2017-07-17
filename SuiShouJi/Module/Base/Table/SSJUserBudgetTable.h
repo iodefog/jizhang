626a639a1fdc2b54c489320ab584ffcf2a743fa7
ricky <125037896@qq.com>
2017-07-17 17:07:19 +0800

    每个表的类

---
+++ SuiShouJi/Module/Base/Table/SSJUserBudgetTable.h
@@ -0,0 +1,34 @@
+//
+//  SSJUserBudgetTable.h
+//  SuiShouJi
+//
+//  Created by ricky on 2017/7/17.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <WCDB/WCDB.h>
+
+@interface SSJUserBudgetTable : NSObject <WCTTableCoding>
+
+/*
+@property(nonatomic, assign) int primary;
+@property(nonatomic, retain) NSString* valueWithIndex;
+@property(nonatomic, assign) int valueWithSpecifiedColumnName;
+@property(nonatomic, retain) NSString* valueWithDefaultValue;
+@property(nonatomic, assign) unsigned int value1WithMultiIndex;
+@property(nonatomic, retain) NSString* value2WithMultiIndex;
+
+//Primary Key
+WCDB_PROPERTY(primary)
+//Property With Index
+WCDB_PROPERTY(valueWithIndex)
+//Propery With Specified Column Name
+WCDB_PROPERTY(valueWithSpecifiedColumnName)
+//Propery With Default Value
+WCDB_PROPERTY(valueWithDefaultValue)
+//Properies With Multi-Indexes
+WCDB_PROPERTY(value1WithMultiIndex)
+WCDB_PROPERTY(value2WithMultiIndex)
+ */
+
+@end
