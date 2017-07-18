a0e14bd68e846f05e2892ac6dec310fe37f2f46f
ricky <125037896@qq.com>
2017-07-18 20:57:45 +0800

    数据合并逻辑

--- SuiShouJi/Components/Business/DataMerge/SSJBaseTableMerge.h
+++ SuiShouJi/Components/Business/DataMerge/SSJBaseTableMerge.h
@@ -6,32 +6,84 @@
 //  Copyright © 2017年 ___9188___. All rights reserved.
 //
 
+// 提示,如果有新表要合并,请在base中统一添加头文件
+
 #import <Foundation/Foundation.h>
-#import "FMDB.h"
+#import <WCDB/WCDB.h>
+#import "SSJUserChargeTable.h"
+#import "SSJMembereChargeTable.h"
+#import "SSJBooksTypeTable.h"
+#import "SSJChargePeriodConfigTable.h"
+#import "SSJUserRemindTable.h"
+#import "SSJUserCreditTable.h"
+#import "SSJLoanTable.h"
+#import "SSJFundInfoTable.h"
+
 
 @interface SSJBaseTableMerge : NSObject
 
+
+typedef NS_ENUM(NSInteger, SSJMergeDataType) {
+    SSJMergeDataTypeByWriteDate,           //  按照修改日期合并
+    SSJMergeDataTypeByWriteBillDate        //  按照记账日期合并
+};
+
+
+/**
+ 表名
+
+ @return 表的名称
+ */
 + (NSString *)tableName;
 
-+ (NSArray *)columns;
-
-+ (NSArray *)primaryKeys;
 
 
+/**
+ 在要合并的表中查出所有需要合并的数据
+
+ @param sourceUserid 合并数据的来源的userid
+ @param targetUserId 合并数据的目标的userid
+ @param mergeType 合并数据的方式(已billid或者以writedate)
+ @param fromDate 合并开始的时间
+ @param toDate 合并结束的时间
+ @param db 数据库
+ @return 字典中有两个值(@"error":为错误,@"results"是查询出来的结果)
+ */
 + (NSDictionary *)queryDatasWithSourceUserId:(NSString *)sourceUserid
                                 TargetUserId:(NSString *)targetUserId
+                                   mergeType:(SSJMergeDataType)mergeType
                                     FromDate:(NSDate *)fromDate
                                       ToDate:(NSDate *)toDate
-                                  inDataBase:(FMDatabase *)db;
+                                  inDataBase:(WCTDatabase *)db;
 
-+ (void)updateIdSourceUserId:(NSString *)sourceUserid
-                TargetUserId:(NSString *)targetUserId
-                     DataDic:(NSDictionary *)dic;
 
-+ (NSDictionary *)queryUserChargeWithUserId:(NSString *)userId
-                                  condition:(NSString *)condition
-                                   FromDate:(NSDate *)fromDate
-                                     ToDate:(NSDate *)toDate
-                                 inDataBase:(FMDatabase *)db;
+/**
+ 查询出所有同名的对应的id
+
+ @param sourceUserid 合并数据的来源的userid
+ @param targetUserId 合并数据的目标的userid
+ @param datas 查出要合并的数据,见上一个方法
+ @param db 数据库
+ @return 字典中以老的id为key,合并的目标用户中的id为value
+ */
++ (NSDictionary *)getSameNameIdsWithSourceUserId:(NSString *)sourceUserid
+                                    TargetUserId:(NSString *)targetUserId
+                                       withDatas:(NSArray *)datas
+                                      inDataBase:(WCTDatabase *)db;
+
+
+/**
+ 更新每个表所关联的表
+
+ @param sourceUserid 合并数据的来源的userid
+ @param targetUserId 合并数据的目标的userid
+ @param datas 上一个方法所产生的字典
+ @param db 数据库
+ @return (BOOL)是否合并成功
+ */
++ (BOOL)updateRelatedTableWithSourceUserId:(NSString *)sourceUserid
+                              TargetUserId:(NSString *)targetUserId
+                                 withDatas:(NSDictionary *)datas
+                                inDataBase:(WCTDatabase *)db;
 
 @end
---
+++ SuiShouJi/Components/Business/DataMerge/SSJUserReminderTableMerge.mm
@@ -0,0 +1,188 @@
+//
+//  SSJUserReminderTableMerge.m
+//  SuiShouJi
+//
+//  Created by ricky on 2017/7/18.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJUserReminderTableMerge.h"
+
+@implementation SSJUserReminderTableMerge
+
++ (NSString *)tableName {
+    return @"BK_USER_REMIND";
+}
+
++ (NSDictionary *)queryDatasWithSourceUserId:(NSString *)sourceUserid
+                                TargetUserId:(NSString *)targetUserId
+                                   mergeType:(SSJMergeDataType)mergeType
+                                    FromDate:(NSDate *)fromDate
+                                      ToDate:(NSDate *)toDate
+                                  inDataBase:(WCTDatabase *)db {
+    
+    // 提醒分成两部分,一部分是信用卡的提醒,一部分是借贷的提醒
+    
+    NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:0];
+    
+    
+    
+    NSMutableArray *tempArr = [NSMutableArray arrayWithCapacity:0];
+    
+    WCTPropertyList multiProperties;
+
+    for (const WCTProperty& property : SSJUserRemindTable.AllProperties) {
+        multiProperties.push_back(property.inTable([self tableName]));
+    }
+    
+    NSString *startDate = [fromDate formattedDateWithFormat:@"yyyy-MM-dd HH:ss:mm.SSS"];
+    
+    NSString *endDate = [toDate formattedDateWithFormat:@"yyyy-MM-dd HH:ss:mm.SSS"];
+    
+    WCTError *error;
+    
+    // 首先查出所有信用卡的提醒
+    WCTMultiSelect *creditRemindSelect;
+    
+    if (mergeType == SSJMergeDataTypeByWriteDate) {
+        creditRemindSelect = [[[db prepareSelectMultiObjectsOnResults:multiProperties
+                                               fromTables:@[ [self tableName], @"bk_user_charge", @"bk_user_credit" ]]
+                   where:SSJUserRemindTable.remindId.inTable([self tableName]) == SSJUserCreditTable.remindId.inTable(@"bk_user_credit")
+                   && SSJUserChargeTable.writeDate.inTable(@"bk_user_charge").between(startDate, endDate)
+                               && SSJUserCreditTable.cardId.inTable(@"bk_user_credit") == SSJUserChargeTable.fundId.inTable(@"bk_user_charge")
+                               && SSJUserRemindTable.userId == sourceUserid]
+                  groupBy:{SSJUserChargeTable.booksId.inTable(@"bk_user_charge")}];
+        
+    } else if (mergeType == SSJMergeDataTypeByWriteBillDate) {
+        creditRemindSelect = [[[db prepareSelectMultiObjectsOnResults:multiProperties
+                                                           fromTables:@[ [self tableName], @"bk_user_charge", @"bk_user_credit" ]]
+                               where:SSJUserRemindTable.remindId.inTable([self tableName]) == SSJUserCreditTable.remindId.inTable(@"bk_user_credit")
+                               && SSJUserChargeTable.billDate.inTable(@"bk_user_charge").between(startDate, endDate)
+                               && SSJUserCreditTable.cardId.inTable(@"bk_user_credit") == SSJUserChargeTable.fundId.inTable(@"bk_user_charge")
+                               && SSJUserRemindTable.userId == sourceUserid]
+                              groupBy:{SSJUserChargeTable.booksId.inTable(@"bk_user_charge")}];
+    }
+    
+    error = creditRemindSelect.error;
+    
+    WCTMultiObject *creditRemindMultiObject;
+    
+    while ((creditRemindMultiObject = [creditRemindSelect nextMultiObject])) {
+        SSJUserRemindTable *userBooks = (SSJUserRemindTable *)[creditRemindMultiObject objectForKey:[self tableName]];
+        [tempArr addObject:userBooks];
+    }
+    
+    // 然后查出借贷的提醒
+    WCTMultiSelect *loanRemindSelect;
+    
+    if (mergeType == SSJMergeDataTypeByWriteDate) {
+        loanRemindSelect = [[[db prepareSelectMultiObjectsOnResults:multiProperties
+                                                           fromTables:@[ [self tableName], @"bk_user_charge", @"bk_user_credit" ]]
+                               where:SSJUserRemindTable.remindId.inTable([self tableName]) == SSJUserCreditTable.remindId.inTable(@"bk_user_credit")
+                               && SSJUserChargeTable.writeDate.inTable(@"bk_user_charge").between(startDate, endDate)
+                             && SSJUserCreditTable.cardId.inTable(@"bk_user_credit") == SSJUserChargeTable.fundId.inTable(@"bk_user_charge")
+                             && SSJUserRemindTable.userId == sourceUserid]
+                              groupBy:{SSJUserRemindTable.remindId.inTable([self tableName])}];
+        
+    } else if (mergeType == SSJMergeDataTypeByWriteBillDate) {
+        loanRemindSelect = [[[db prepareSelectMultiObjectsOnResults:multiProperties
+                                                           fromTables:@[ [self tableName], @"bk_user_charge", @"bk_loan" ]]
+                               where:SSJUserRemindTable.remindId.inTable([self tableName]) == SSJLoanTable.remindId.inTable(@"bk_loan")
+                               && SSJUserChargeTable.billDate.inTable(@"bk_user_charge").between(startDate, endDate)
+                             && SSJUserCreditTable.cardId.inTable(@"bk_loan") == SSJUserChargeTable.fundId.inTable(@"bk_user_charge")
+                             && SSJUserRemindTable.userId == sourceUserid]
+                            groupBy:{SSJUserRemindTable.remindId.inTable([self tableName])}];
+    }
+    
+    error = loanRemindSelect.error;
+    
+    WCTMultiObject *loanRemindMultiObject;
+    
+    while ((loanRemindMultiObject = [loanRemindSelect nextMultiObject])) {
+        SSJUserRemindTable *remind = (SSJUserRemindTable *)[loanRemindMultiObject objectForKey:[self tableName]];
+        [tempArr addObject:remind];
+    }
+    
+    [dict setObject:tempArr forKey:@"results"];
+    
+    [dict setObject:error forKey:@"error"];
+    
+    return dict;
+}
+
++ (NSDictionary *)getSameNameIdsWithSourceUserId:(NSString *)sourceUserid
+                                    TargetUserId:(NSString *)targetUserId
+                                       withDatas:(NSArray *)datas
+                                      inDataBase:(WCTDatabase *)db {
+    
+    // 建立一个新老id对照的字典,key是老的id,value是新的id
+    NSMutableDictionary *newAndOldIdDic = [NSMutableDictionary dictionaryWithCapacity:0];
+    
+    [datas enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        SSJUserRemindTable *currentRemind = (SSJUserRemindTable *)obj;
+        
+        SSJUserRemindTable *sameRemind = [[db getOneObjectOfClass:SSJUserRemindTable.class
+                                                        fromTable:[self tableName]]
+                                          
+                                          where:SSJUserRemindTable.remindName == currentRemind.remindName];
+        
+        [newAndOldIdDic setObject:currentRemind.remindName forKey:sameRemind.remindName];
+        
+    }];
+    
+    return newAndOldIdDic;
+}
+
++ (BOOL)updateRelatedTableWithSourceUserId:(NSString *)sourceUserid
+                              TargetUserId:(NSString *)targetUserId
+                                 withDatas:(NSDictionary *)datas
+                                inDataBase:(WCTDatabase *)db {
+    
+    __block BOOL success = NO;
+    
+    // 和流水有关的表:信用卡,借贷
+    [datas enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
+        NSString *newId = obj;
+        NSString *oldId = key;
+        if (![db isTableExists:@"temp_user_credit"] || ![db isTableExists:@"temp_loan"]) {
+            SSJPRINT(@">>>>>>>>提醒所关联的表不存在<<<<<<<<");
+            *stop = NO;
+            success = NO;
+        }
+        
+        // 更新信用卡表
+        SSJUserCreditTable *credit = [[SSJUserCreditTable alloc] init];
+        credit.remindId = newId;
+        success = [db updateRowsInTable:@"temp_user_credit"
+                           onProperties:SSJUserCreditTable.remindId
+                             withObject:credit
+                                  where:SSJUserCreditTable.remindId == oldId];
+        if (!success) {
+            *stop = NO;
+        }
+        
+        // 更新借贷
+        SSJLoanTable *loan = [[SSJLoanTable alloc] init];
+        loan.remindId = newId;
+        success = [db updateRowsInTable:@"temp_loan"
+                           onProperties:SSJLoanTable.remindId
+                             withObject:loan
+                                  where:SSJLoanTable.remindId == oldId];
+        
+        if (!success) {
+            *stop = NO;
+        }
+        
+        // 删除同名的提醒
+        success = [db deleteObjectsFromTable:[self tableName]
+                                       where:SSJUserChargeTable.chargeId == oldId];
+        if (!success) {
+            *stop = NO;
+        }
+    }];
+    
+    return success;
+}
+
+
+@end
