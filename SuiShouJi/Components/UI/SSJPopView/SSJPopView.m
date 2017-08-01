10558d06e1f9ae51e984a9dcc3a436e3568f29ae
ricky <125037896@qq.com>
2017-08-01 10:18:57 +0800

    选择弹窗

---
+++ SuiShouJi/Components/UI/SSJPopView/SSJPopView.m
@@ -0,0 +1,217 @@
+//
+//  SSJPopView.m
+//  SuiShouJi
+//
+//  Created by ricky on 2017/8/1.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJPopView.h"
+#import "SSJBaseTableViewCell.h"
+
+static NSString *kCellID = @"cellID";
+
+@interface SSJPopView()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic, strong) NSArray *titles;
+
+@property (nonatomic, strong) NSArray *images;
+
+@property (nonatomic, strong) UITableView *tableView;
+
+@property (nonatomic, strong) UIImageView *accessoryView;
+
+@property (nonatomic, strong) UILabel *titleLab;
+
+@property (nonatomic) NSInteger selectIndex;
+
+@property (nonatomic, strong) UIView *seperatorView;
+
+@end
+
+
+@implementation SSJPopView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addSubview:self.tableView];
+        [self addSubview:self.titleLab];
+        [self addSubview:self.seperatorView];
+        self.layer.cornerRadius = 12;
+        self.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.secondaryFillColor];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateCellAppearanceAfterThemeChanged) name:SSJThemeDidChangeNotification object:nil];
+    }
+    return self;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)updateConstraints {
+    [self.titleLab mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self).offset(20);
+        make.centerX.mas_equalTo(self);
+    }];
+    
+    [self.tableView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(self);
+        make.left.mas_equalTo(self);
+        make.top.mas_equalTo(self).offset(55);
+        make.bottom.mas_equalTo(self);
+    }];
+    
+    [self.seperatorView mas_updateConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.tableView);
+        make.centerX.mas_equalTo(self);
+        make.width.mas_equalTo(self);
+        make.height.mas_equalTo(1);
+    }];
+    
+    [super updateConstraints];
+}
+
+- (void)showWithSelectedIndex:(NSInteger)index {
+    if (self.superview) {
+        return;
+    }
+    
+    self.selectIndex = index;
+    
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+    
+    self.top = keyWindow.height;
+    
+    self.centerX = keyWindow.centerX;
+    
+    [keyWindow ssj_showViewWithBackView:self backColor:[UIColor blackColor] alpha:0.3 target:self touchAction:@selector(dismiss) animation:^{
+        self.center = keyWindow.center;
+    } timeInterval:0.25 fininshed:NULL];
+}
+
+- (void)dismiss {
+    if (!self.superview) {
+        return;
+    }
+    
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+    
+    [self.superview ssj_hideBackViewForView:self animation:^{
+        self.top = keyWindow.bottom;
+    } timeInterval:0.25 fininshed:NULL];
+}
+
+#pragma mark - Getter
+- (UILabel *)titleLab {
+    if (!_titleLab) {
+        _titleLab = [[UILabel alloc] init];
+        _titleLab.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor];
+        _titleLab.font = [UIFont systemFontOfSize:SSJ_FONT_SIZE_3];
+    }
+    return _titleLab;
+}
+
+- (UITableView *)tableView{
+    if (_tableView == nil) {
+        _tableView = [[UITableView alloc] initWithFrame:CGRectZero];
+        _tableView.rowHeight = 55;
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        _tableView.backgroundView = nil;
+        _tableView.backgroundColor = [UIColor clearColor];
+        _tableView.separatorColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha];
+        [_tableView ssj_clearExtendSeparator];
+        if ([_tableView respondsToSelector:@selector(setSeparatorInset:)]) {
+            [_tableView setSeparatorInset:UIEdgeInsetsZero];
+        }
+    }
+    return _tableView;
+}
+
+- (UIImageView *)accessoryView {
+    if (!_accessoryView) {
+        _accessoryView = [[UIImageView alloc] initWithImage:[[UIImage imageNamed:@"checkmark"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate]];
+    }
+    return _accessoryView;
+}
+
+- (UIView *)seperatorView {
+    if (!_seperatorView) {
+        _seperatorView = [[UIView alloc] init];
+        _seperatorView.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha];
+    }
+    return _seperatorView;
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _titles.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *title = [_titles ssj_safeObjectAtIndex:indexPath.row];
+    
+    NSString *image = [_images ssj_safeObjectAtIndex:indexPath.row];
+
+    
+    SSJBaseTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellID];
+    if (!cell) {
+        cell = [[SSJBaseTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kCellID];
+        cell.backgroundColor = [UIColor clearColor];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    
+    cell.textLabel.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor];
+    
+    cell.textLabel.font = [UIFont ssj_pingFangRegularFontOfSize:SSJ_FONT_SIZE_3];
+    
+    cell.textLabel.text = title;
+    
+    cell.imageView.image = [UIImage imageNamed:image];
+    
+    cell.accessoryView = indexPath.row == self.selectIndex ? self.accessoryView : nil;
+    
+    self.accessoryView.tintColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.marcatoColor];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    self.selectIndex = indexPath.row;
+    if (self.didSelectAtIndexBlock) {
+        self.didSelectAtIndexBlock(indexPath.row);
+    }
+    [self dismiss];
+    [self.tableView reloadData];
+}
+
+#pragma mark - Setter
+- (void)setTitles:(NSArray *)titles andImages:(NSArray *)images {
+    self.titles = titles;
+    self.images = images;
+    [self.tableView reloadData];
+}
+
+- (void)setTitle:(NSString *)title {
+    self.titleLab.text = title;
+}
+
+#pragma mark - Private
+- (void)updateCellAppearanceAfterThemeChanged {
+    self.titleLab.textColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.mainColor];
+    self.seperatorView.backgroundColor = [UIColor ssj_colorWithHex:SSJ_CURRENT_THEME.cellSeparatorColor alpha:SSJ_CURRENT_THEME.cellSeparatorAlpha];
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
