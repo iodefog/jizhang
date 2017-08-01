db6a9250a9aa2ef73aa4fb97c10f35e82587b04b
ricky <125037896@qq.com>
2017-08-01 10:57:16 +0800

    改打测试环境

---
+++ SuiShouJi/Module/Financing/FinancingHome/View/SSJMergeFundSelectView.h
@@ -0,0 +1,16 @@
+//
+//  SSJMergeFundSelectView.h
+//  SuiShouJi
+//
+//  Created by ricky on 2017/8/1.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SSJMergeFundSelectView : UIView
+
+@property (nonatomic, strong) NSArray <SSJFundInfoTable *> *fundsArr;
+
+@end
