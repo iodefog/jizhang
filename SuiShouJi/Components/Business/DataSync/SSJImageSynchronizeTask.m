8d936af82fefc2a869e63bf2feb1b0ccaf8b90b1
old lang <langxintong@9188.com>
2017-08-10 20:32:08 +0800

    修复图片同步bug

--- SuiShouJi/Components/Business/DataSync/SSJImageSynchronizeTask.m
+++ SuiShouJi/Components/Business/DataSync/SSJImageSynchronizeTask.m
@@ -14,6 +14,50 @@ static NSString *const kSyncImagePrivateKey = @"iwannapie?!";
 static NSString *const kImageNameKey = @"kImageNameKey";
 static NSString *const kSyncTypeKey = @"kSyncTypeKey";
 
+@interface _SSJSyncFileModel : NSObject
+
+@property (nonatomic, strong) NSData *fileData;
+
+@property (nonatomic, copy) NSString *fileName;
+
+@property (nonatomic, copy) NSString *mimeType;
+
+@end
+
+@implementation _SSJSyncFileModel
+
++ (instancetype)modelWithFileData:(NSData *)data fileName:(NSString *)name mimeType:(NSString *)type {
+    _SSJSyncFileModel *model = [[_SSJSyncFileModel alloc] init];
+    model.fileData = data;
+    model.fileName = name;
+    model.mimeType = type;
+    return model;
+}
+
+@end
+
+@interface _SSJSyncFileCompoundModel : NSObject
+
+@property (nonatomic, strong) _SSJSyncFileModel *imageModel;
+
+@property (nonatomic, strong) _SSJSyncFileModel *thumbImageModel;
+
+@property (nonatomic, copy) NSDictionary *params;
+
+@end
+
+@implementation _SSJSyncFileCompoundModel
+
++ (instancetype)modelWithImageModel:(_SSJSyncFileModel *)imageModel thumbImageModel:(_SSJSyncFileModel *)thumbImageModel params:(NSDictionary *)params {
+    _SSJSyncFileCompoundModel *compoundModel = [[_SSJSyncFileCompoundModel alloc] init];
+    compoundModel.imageModel = imageModel;
+    compoundModel.thumbImageModel = thumbImageModel;
+    compoundModel.params = params;
+    return compoundModel;
+}
+
+@end
+
 @interface SSJImageSynchronizeTask ()
 
 @property (nonatomic) NSInteger uploadCounter;
@@ -30,7 +74,6 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
 - (void)startSyncWithSuccess:(void (^)(void))success failure:(void (^)(NSError *error))failure {
     __block NSError *tError = nil;
     NSArray *syncImages = [self getImagesNeedToSyncWithError:&tError];
-    
     if (tError) {
         if (failure) {
             failure(tError);
@@ -45,46 +88,18 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
         return;
     }
     
-    self.uploadCounter = syncImages.count;
-    
     // 遍历未同步的图片名称，并上传
-    for (int i = 0; i < syncImages.count; i++) {
-        NSDictionary *imageInfo = syncImages[i];
-        
-        NSString *imageName = imageInfo[kImageNameKey];
-        NSString *syncType = imageInfo[kSyncTypeKey];
-        NSString *userId = self.userId;
-        NSString *thumbImgName = [self thumbImgNameWithImgName:imageName];
-        NSString *sign = [[NSString stringWithFormat:@"%@%@%@%@%@", userId, imageName, thumbImgName, syncType, kSyncImagePrivateKey] ssj_md5HexDigest];
-        
-        NSDictionary *params = @{@"cuserId":userId,
-                                 @"imageName":imageName,
-                                 @"thumbName":thumbImgName,
-                                 @"syncType":syncType,
-                                 @"sign":sign,
-                                 @"appVersion":SSJAppVersion()};
-        
-        NSMutableArray *imageList = [NSMutableArray arrayWithCapacity:2];
-        NSData *imgData = [NSData dataWithContentsOfFile:SSJImagePath(imageName)];
-        NSData *thumbData = [NSData dataWithContentsOfFile:SSJImagePath(thumbImgName)];
-        
-        if (imgData) {
-            [imageList addObject:[SSJSyncFileModel modelWithFileData:imgData fileName:imageName mimeType:@"image/jpeg"]];
+    NSArray *imgModels = [self organiseImgCompoundModelsWithImgInfos:syncImages];
+    if (imgModels.count == 0) {
+        if (success) {
+            success();
         }
-        
-        if (thumbData) {
-            [imageList addObject:[SSJSyncFileModel modelWithFileData:thumbData fileName:thumbImgName mimeType:@"image/jpeg"]];
-        }
-        
-        if (imageList.count == 0) {
-            if (failure) {
-                failure([NSError errorWithDomain:SSJErrorDomain code:SSJErrorCodeNoImageSyncNeedToSync userInfo:@{NSLocalizedDescriptionKey:@"没有需要同步的图片"}]);
-            }
-            return;
-        }
-        
-        SSJPRINT(@"<<< ------- 图片同步开始! ------- >>>");
-        [self uploadModelList:imageList headerParams:params toUrlPath:@"/sync/syncimg.go" completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
+        return;
+    }
+    
+    self.uploadCounter = imgModels.count;
+    for (_SSJSyncFileCompoundModel *compoundModel in imgModels) {
+        [self uploadImgCompoundModel:compoundModel headerParams:compoundModel.params toUrlPath:@"/sync/syncimg.go" completionHandler:^(NSURLResponse *response, id responseObject, NSError *error) {
             dispatch_async(self.syncQueue, ^{
                 
                 self.uploadCounter --;
@@ -132,24 +147,18 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
                 }];
                 
                 // 删除已上传的图片
-                [[NSFileManager defaultManager] removeItemAtPath:SSJImagePath(uploadImgName) error:nil];
-                [[NSFileManager defaultManager] removeItemAtPath:SSJImagePath([self thumbImgNameWithImgName:uploadImgName]) error:nil];
+//                [[NSFileManager defaultManager] removeItemAtPath:SSJImagePath(uploadImgName) error:nil];
+//                [[NSFileManager defaultManager] removeItemAtPath:SSJImagePath([self thumbImgNameWithImgName:uploadImgName]) error:nil];
                 
                 // 上传完最后一组图片后根据过程中是否有错误，调用响应的回调
-                if (i == syncImages.count - 1) {
+                if (self.uploadCounter == 0) {
                     if (tError) {
-                        SSJPRINT(@"<<< ------- 图片同步失败! ------- >>>");
-                        if (self.uploadCounter == 0) {
-                            if (failure) {
-                                failure(tError);
-                            }
+                        if (failure) {
+                            failure(tError);
                         }
                     } else {
-                        SSJPRINT(@"<<< ------- 图片同步成功！------- >>>");
-                        if (self.uploadCounter == 0) {
-                            if (success) {
-                                success();
-                            }
+                        if (success) {
+                            success();
                         }
                     }
                 }
@@ -162,34 +171,15 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
     NSMutableArray *imageInfoArr = [NSMutableArray array];
     [[SSJDatabaseQueue sharedInstance] inDatabase:^(FMDatabase *db) {
         // 查询当前用户的流水表中没有同步的图片
-        FMResultSet *resultSet = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_user_charge as b where a.rid = b.ichargeid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
-        if (!resultSet) {
+        FMResultSet *rs = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_user_charge as b where a.rid = b.ichargeid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
+        if (!rs) {
             *error = [db lastError];
             return;
         }
         
-        while ([resultSet next]) {
-            NSString *imageName = [resultSet stringForColumn:@"cimgname"];
-            if (![imageName hasSuffix:@".jpg"]) {
-                imageName = [NSString stringWithFormat:@"%@.jpg",imageName];
-            }
-            NSString *syncType = [resultSet stringForColumn:@"isynctype"];
-            if (imageName.length) {
-                [imageInfoArr addObject:@{kImageNameKey:imageName,
-                                          kSyncTypeKey:syncType}];
-            }
-        }
-        
-        // 查询当前用户的顶起记账中没有同步的图片
-        resultSet = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_charge_period_config as b where a.rid = b.iconfigid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
-        if (!resultSet) {
-            *error = [db lastError];
-            return;
-        }
-        
-        while ([resultSet next]) {
-            NSString *imageName = [resultSet stringForColumn:@"cimgname"];
-            NSString *syncType = [resultSet stringForColumn:@"isynctype"];
+        while ([rs next]) {
+            NSString *imageName = [rs stringForColumn:@"cimgname"];
+            NSString *syncType = [rs stringForColumn:@"isynctype"];
             if (![imageName hasSuffix:@".jpg"]) {
                 imageName = [NSString stringWithFormat:@"%@.jpg",imageName];
             }
@@ -198,21 +188,107 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
                                           kSyncTypeKey:syncType}];
             }
         }
+        [rs close];
+        
+        // 查询当前用户的周期记账中没有同步的图片
+        rs = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_charge_period_config as b where a.rid = b.iconfigid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
+        if (!rs) {
+            *error = [db lastError];
+            return;
+        }
+        
+        while ([rs next]) {
+            NSString *imageName = [rs stringForColumn:@"cimgname"];
+            NSString *syncType = [rs stringForColumn:@"isynctype"];
+            if (![imageName hasSuffix:@".jpg"]) {
+                imageName = [NSString stringWithFormat:@"%@.jpg",imageName];
+            }
+            if (imageName.length) {
+                [imageInfoArr addObject:@{kImageNameKey:imageName,
+                                          kSyncTypeKey:syncType}];
+            }
+        }
+        [rs close];
+        
+        // 查询心愿需要同步的图片
+        rs = [db executeQuery:@"select a.cimgname, a.isynctype from bk_img_sync as a, bk_wish as b where a.rid = b.wishid and a.operatortype <> 2 and a.isyncstate = 0 and b.cuserid = ?", self.userId];
+        if (!rs) {
+            *error = [db lastError];
+            return;
+        }
+        
+        while ([rs next]) {
+            NSString *imageName = [rs stringForColumn:@"cimgname"];
+            NSString *syncType = [rs stringForColumn:@"isynctype"];
+            if (![imageName hasSuffix:@".jpg"]) {
+                imageName = [NSString stringWithFormat:@"%@.jpg",imageName];
+            }
+            if (imageName.length) {
+                [imageInfoArr addObject:@{kImageNameKey:imageName,
+                                          kSyncTypeKey:syncType}];
+            }
+        }
+        [rs close];
     }];
     return imageInfoArr;
 }
 
-- (NSURLSessionUploadTask *)uploadModelList:(NSArray<SSJSyncFileModel *> *)modelList headerParams:(NSDictionary *)prarms toUrlPath:(NSString *)path completionHandler:(void (^)(NSURLResponse *response, id responseObject, NSError *error))completionHandler {
-    
+- (NSArray *)organiseImgCompoundModelsWithImgInfos:(NSArray *)imgInfos {
+    NSMutableArray *imgModels = [NSMutableArray array];
+    for (int i = 0; i < imgInfos.count; i++) {
+        NSDictionary *imageInfo = imgInfos[i];
+        
+        NSString *imageName = imageInfo[kImageNameKey];
+        NSString *syncType = imageInfo[kSyncTypeKey];
+        NSString *userId = self.userId;
+        NSString *thumbImgName = [self thumbImgNameWithImgName:imageName];
+        NSString *sign = [[NSString stringWithFormat:@"%@%@%@%@%@", userId, imageName, thumbImgName, syncType, kSyncImagePrivateKey] ssj_md5HexDigest];
+        
+        NSDictionary *params = @{@"cuserId":userId,
+                                 @"imageName":imageName,
+                                 @"thumbName":thumbImgName,
+                                 @"syncType":syncType,
+                                 @"sign":sign,
+                                 @"appVersion":SSJAppVersion()};
+        
+        NSData *imgData = [NSData dataWithContentsOfFile:SSJImagePath(imageName)];
+        NSData *thumbData = [NSData dataWithContentsOfFile:SSJImagePath(thumbImgName)];
+        if (!imgData && !thumbData) {
+            continue;
+        }
+        
+        _SSJSyncFileModel *imgModel = [_SSJSyncFileModel modelWithFileData:imgData
+                                                                  fileName:imageName
+                                                                  mimeType:@"image/jpeg"];
+        
+        _SSJSyncFileModel *thumbImgModel = [_SSJSyncFileModel modelWithFileData:thumbData
+                                                                       fileName:thumbImgName
+                                                                       mimeType:@"image/jpeg"];
+        
+        _SSJSyncFileCompoundModel *compoundModel = [_SSJSyncFileCompoundModel modelWithImageModel:imgModel
+                                                                                  thumbImageModel:thumbImgModel
+                                                                                           params:params];
+        [imgModels addObject:compoundModel];
+    }
+    return imgModels;
+}
+
+- (NSURLSessionUploadTask *)uploadImgCompoundModel:(_SSJSyncFileCompoundModel *)compoundModel
+                                      headerParams:(NSDictionary *)prarms
+                                         toUrlPath:(NSString *)path
+                                 completionHandler:(void (^)(NSURLResponse *response, id responseObject, NSError *error))completionHandler {
     //  创建请求
     NSError *tError = nil;
     NSMutableURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:@"POST" URLString:SSJURLWithAPI(path) parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-        for (int i = 0; i < modelList.count; i ++) {
-            SSJSyncFileModel *model = [modelList ssj_safeObjectAtIndex:i];
-            if (model.fileData && model.fileName && model.mimeType) {
-                [formData appendPartWithFileData:model.fileData name:model.fileName fileName:model.fileName mimeType:model.mimeType];
-            }
-        }
+        [formData appendPartWithFileData:compoundModel.imageModel.fileData
+                                    name:compoundModel.imageModel.fileName
+                                fileName:compoundModel.imageModel.fileName
+                                mimeType:compoundModel.imageModel.mimeType];
+        
+        [formData appendPartWithFileData:compoundModel.thumbImageModel.fileData
+                                    name:compoundModel.thumbImageModel.fileName
+                                fileName:compoundModel.thumbImageModel.fileName
+                                mimeType:compoundModel.thumbImageModel.mimeType];
     } error:&tError];
     
     if (tError) {
@@ -226,7 +302,7 @@ static NSString *const kSyncTypeKey = @"kSyncTypeKey";
         [request setValue:obj forHTTPHeaderField:key];
     }];
     
-    //  开始上传
+    // 开始上传
     NSURLSessionUploadTask *task = [self.sessionManager uploadTaskWithStreamedRequest:request progress:nil completionHandler:completionHandler];
     [task resume];
     
