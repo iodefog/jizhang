7158b259b93724dde8b8025d9c98f673db7d2c4c
old lang <langxintong@9188.com>
2017-08-21 18:43:42 +0800

    更新数据库版本

---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion18.h
@@ -0,0 +1,13 @@
+//
+//  SSJDatabaseVersion18.h
+//  SuiShouJi
+//
+//  Created by old lang on 2017/8/21.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSJDatabaseVersion18 : NSObject
+
+@end
---
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion18.m
@@ -0,0 +1,13 @@
+//
+//  SSJDatabaseVersion18.m
+//  SuiShouJi
+//
+//  Created by old lang on 2017/8/21.
+//  Copyright © 2017年 ___9188___. All rights reserved.
+//
+
+#import "SSJDatabaseVersion18.h"
+
+@implementation SSJDatabaseVersion18
+
+@end
