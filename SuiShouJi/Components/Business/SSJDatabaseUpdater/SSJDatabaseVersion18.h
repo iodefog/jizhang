ee5e42aad7f0a6fb345f25332dac25941aa77a00
old lang <langxintong@9188.com>
2017-08-30 15:23:48 +0800

    升级时修改借贷流水的cid

--- SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion18.h
+++ SuiShouJi/Components/Business/SSJDatabaseUpdater/SSJDatabaseVersion18.h
@@ -9,6 +9,20 @@
 #import <Foundation/Foundation.h>
 #import "SSJDatabaseVersionProtocol.h"
 
+@class SSJLoanChargeModel;
+
 @interface SSJDatabaseVersion18 : NSObject <SSJDatabaseVersionProtocol>
+
+/**
+ 修改借贷流水的cid，cid拼接格式：借贷项目id_idx；idx从1开始
+
+ @param models 存储借贷流水模型的数组
+ @param db 数据库对象
+ @param error 错误描述对象
+ @return 是否修改成功
+ */
++ (BOOL)updateLoanChargesWithModels:(NSArray<SSJLoanChargeModel *> *)models
+                           database:(FMDatabase *)db
+                              error:(NSError **)error;
 
 @end
